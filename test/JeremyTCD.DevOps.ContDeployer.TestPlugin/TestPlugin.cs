846f0f56f9864c8aa604f604878bdd26e8c9a21f
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-01 20:28:48 +0800

    Added JeremyTCD.DevOps.ContDeployer.TestPlugin

---
+++ test/JeremyTCD.DevOps.ContDeployer.TestPlugin/TestPlugin.cs
@@ -0,0 +1,16 @@
+﻿using JeremyTCD.DevOps.ContDeployer;
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Text;
+
+namespace JeremyTCD.DevOps.TestPlugin
+{
+    [Export(typeof(IPlugin))]
+    public class TestPlugin : IPlugin
+    {
+        public void Execute()
+        {
+        }
+    }
+}
