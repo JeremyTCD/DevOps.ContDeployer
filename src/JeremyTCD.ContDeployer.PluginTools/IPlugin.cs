2adc93315ac72e341446235e72f78e83c33cc88f
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-05 12:34:08 +0800

    Added DefaultConfig property to IPlugin

--- src/JeremyTCD.ContDeployer.PluginTools/IPlugin.cs
+++ src/JeremyTCD.ContDeployer.PluginTools/IPlugin.cs
@@ -6,6 +6,8 @@ namespace JeremyTCD.ContDeployer.PluginTools
 {
     public interface IPlugin
     {
+        IDictionary<string, Object> DefaultConfig { get; set; }
+
         void Run(IDictionary<string, object> config, PipelineContext context, LinkedList<PipelineStep> steps);
     }
 }
