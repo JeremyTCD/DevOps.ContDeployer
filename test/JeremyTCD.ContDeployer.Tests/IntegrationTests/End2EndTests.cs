4119709aceb3217cd401024263306ac8a97e6242
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-05 22:00:50 +0800

    Added integration tests for ContDeployer

---
+++ test/JeremyTCD.ContDeployer.Tests/IntegrationTests/End2EndTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using JeremyTCD.ContDeployer.PluginTools;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JeremyTCD.ContDeployer.Tests.IntegrationTests
+{
+    [Collection(nameof(E2ECollection))]
+    public class End2EndTests
+    {
+        private string _tempDir { get; }
+        private string _tempPluginsDir { get; }
+
+        public End2EndTests(E2EFixture fixture)
+        {
+            _tempDir = fixture.TempDir;
+            _tempPluginsDir = fixture.TempPluginsDir;
+            fixture.ResetTempDir();
+        }
+
+        [Fact]
+        public void DefaultPipline_GeneratesTagGithubReleaseAndPublishesIfNewVersionHasBeenAdded()
+        {
+            // Arrange
+            object options = new
+            {
+                Pipeline = new
+                {
+                    PipelineSteps = new PipelineStep[] {
+                                new PipelineStep {PluginName="LogMetadataFactory"}
+                            }
+                }
+            };
+            string optionsJson = JsonConvert.SerializeObject(options);
+            File.WriteAllText("cd.json", optionsJson);
+
+            // Act
+            ContDeployer.Main(null);
+
+            // Assert
+        }
+    }
+}
---
+++ test/JeremyTCD.ContDeployer.Tests/IntegrationTests/Shared.cs
@@ -0,0 +1,52 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace JeremyTCD.ContDeployer.Tests.IntegrationTests
+{
+    [CollectionDefinition(nameof(E2ECollection))]
+    public class E2ECollection : ICollectionFixture<E2EFixture>
+    {
+    }
+
+    public class E2EFixture : IDisposable
+    {
+        public string TempDir { get; }
+        public string TempPluginsDir { get; }
+
+        public E2EFixture()
+        {
+            TempDir = Path.Combine(Path.GetTempPath(), "ContDeployerTemp");
+            TempPluginsDir = Path.Combine(TempDir, "plugins");
+        }
+
+        // Deletes entire temp directory, recreates it and inits git repository
+        public void ResetTempDir()
+        {
+            // instead of setting current directory, add a directory option to cd.json
+            // so devenv doesn't fuck up tests. 
+            Directory.SetCurrentDirectory("\\");
+
+            if (Directory.Exists(TempDir))
+            {
+                Directory.Delete(TempDir, true);
+            }
+            Directory.CreateDirectory(TempDir);
+            Directory.CreateDirectory(TempPluginsDir);
+
+            Repository.Init(TempDir);
+
+            Directory.SetCurrentDirectory(TempDir);
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(TempDir, true);
+            Directory.SetCurrentDirectory("\\");
+        }
+    }
+}
+
