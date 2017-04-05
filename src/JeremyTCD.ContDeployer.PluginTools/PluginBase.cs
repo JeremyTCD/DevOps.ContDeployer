39a40f792ed21f61368eb0f715c2802517e6a2ca
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-05 12:35:19 +0800

    Added abstract class PluginBase
    - Added test project for PluginTools and a test for PluginBase.CombineConfigs

---
+++ src/JeremyTCD.ContDeployer.PluginTools/PluginBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JeremyTCD.ContDeployer.PluginTools
+{
+    public abstract class PluginBase : IPlugin
+    {
+        public abstract IDictionary<string, object> DefaultConfig { get; set; }
+
+        public abstract void Run(IDictionary<string, object> config, PipelineContext context, LinkedList<PipelineStep> steps);
+
+        /// <summary>
+        /// Combines two <see cref="IDictionary{string, object}"/> instances, ignoring duplicates. <paramref name="primaryConfig"/> values take
+        /// precedence.
+        /// </summary>
+        /// <param name="primaryConfig"></param>
+        /// <param name="secondaryConfig"></param>
+        /// <returns></returns>
+        public IDictionary<string, object> CombineConfigs(IDictionary<string, object> primaryConfig, IDictionary<string, object> secondaryConfig)
+        {
+            Dictionary<string, object> result = new Dictionary<string, object>();
+
+            secondaryConfig.ToList().ForEach(x => result[x.Key] = x.Value);
+            primaryConfig.ToList().ForEach(x => result[x.Key] = x.Value);
+
+            return result;
+        }
+    }
+}
---
+++ test/PluginTools.Tests/PluginBaseTests.cs
@@ -0,0 +1,47 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using System;
+using Xunit;
+using System.Collections.Generic;
+
+namespace PluginTools.Tests
+{
+    public class PluginBaseTests
+    {
+        [Fact]
+        public void CombineConfigs_CombinesConfigs()
+        {
+            // Arrange
+            Dictionary<string, object> primaryConfig = new Dictionary<string, object>()
+            {
+                {"key1", "primaryValue1" },
+                {"key3", "primaryValue2" }
+            };
+            Dictionary<string, object> secondaryConfig = new Dictionary<string, object>()
+            {
+                {"key1", "secondaryValue1" },
+                {"key2", "secondaryValue2" }
+            };
+
+            TestPlugin testPlugin = new TestPlugin();
+
+            // Act
+            IDictionary<string, object> result = testPlugin.CombineConfigs(primaryConfig, secondaryConfig);
+
+            // Assert
+            Assert.Equal(3, result.Count);
+            Assert.Equal(primaryConfig["key1"], result["key1"]);
+            Assert.Equal(secondaryConfig["key2"], result["key2"]);
+            Assert.Equal(primaryConfig["key3"], result["key3"]);
+        }
+
+        public class TestPlugin : PluginBase
+        {
+            public override IDictionary<string, object> DefaultConfig { get; set; }
+
+            public override void Run(IDictionary<string, object> config, PipelineContext context, LinkedList<PipelineStep> steps)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
