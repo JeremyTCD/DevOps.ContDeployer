ab6d76540822a8fefe0c41cf09a5d75791d64c03
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-06 21:30:39 +0800

    Added logic for retrieving and comparing commits

--- src/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory/LogMetadataFactory.cs
+++ src/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory/LogMetadataFactory.cs
@@ -3,6 +3,7 @@ using System;
 using System.Composition;
 using System.Collections.Generic;
 using LibGit2Sharp;
+using System.Linq;
 
 namespace JeremyTCD.ContDeployer.Plugin.LogMetadataFactory
 {
@@ -11,16 +12,54 @@ namespace JeremyTCD.ContDeployer.Plugin.LogMetadataFactory
     {
         public override IDictionary<string, object> DefaultConfig { get; set; } = new Dictionary<string, object>
         {
-            { "fileName", "changelog.md" },
+            { "fileName", "changelog" },
             { "branch", "master" }
         };
 
         public override void Run(IDictionary<string, object> config, PipelineContext context, LinkedList<PipelineStep> steps)
         {
             config = CombineConfigs(config, DefaultConfig);
+            string fileName = (string) config["filename"];
+            string branch = (string)config["branch"];
 
-            // TODO: create integration test with dummy folder so plugin can be stepped through
+            // TODO what if you're working another branch
+            List<Commit> commits = context.
+                Repository.
+                Commits.
+                QueryBy(new CommitFilter() { SortBy = CommitSortStrategies.Topological }).
+                ToList();
 
+            if (commits.Count == 0)
+            {
+                throw new Exception($"{nameof(LogMetadataFactory)}: Repository has no commits");
+            }
+
+            // Get trees for head and previous commit
+            Commit head = commits[0];
+            Tree newTree = head.Tree;
+            Commit previous = commits.Count == 1 ? null : commits[1];
+            Tree oldTree = previous?.Tree;
+
+            // Check whether changelog has been added or modified
+            bool changelogChanged = false;
+            if(commits.Count == 1)
+            {
+                changelogChanged = newTree.ToList().Any(treeEntry => treeEntry.Path == fileName);
+            }
+            else 
+            {
+                TreeChanges changes = context.Repository.Diff.Compare<TreeChanges>(oldTree, newTree, new string[] { fileName });
+                changelogChanged = changes.
+                    ToList().
+                    Any(treeEntryChanges => treeEntryChanges.Path == fileName && (treeEntryChanges.Status == ChangeKind.Modified || treeEntryChanges.Status == ChangeKind.Added));
+            }
+            if (!changelogChanged)
+            {
+                return;
+            }
+
+
+            Console.WriteLine(newTree.Id);
             // Check if changelog changed
             //  - get changed files using git show: Invoke-Git show --pretty="" --name-only $commit
             //  - check if changelog changed
@@ -46,11 +85,6 @@ namespace JeremyTCD.ContDeployer.Plugin.LogMetadataFactory
             //    Commands.Checkout(repo, "master");
 
             //}
-
-
-
-
-            throw new NotImplementedException();
         }
     }
 }
