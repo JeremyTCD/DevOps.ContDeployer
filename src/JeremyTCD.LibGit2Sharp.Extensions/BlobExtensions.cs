ef5a2d0432724752e2307693c3f445aa4cdb46e9
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-07 22:14:19 +0800

    Added LibGit2Sharp.Extensions project
    - Should move this to its own package eventually

---
+++ src/JeremyTCD.LibGit2Sharp.Extensions/BlobExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace LibGit2Sharp.Extensions
+{
+    public static class BlobExtensions
+    {
+        public static string ReadAsString(this Blob blob)
+        {
+            if (blob == null)
+            {
+                throw new ArgumentNullException(nameof(blob));
+            }
+
+            Stream contentStream = blob.GetContentStream();
+
+            using (StreamReader reader = new StreamReader(contentStream, Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
