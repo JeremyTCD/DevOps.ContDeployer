64c63159488294f9c685a1c23eeee2df26a68d6f
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-07 22:41:18 +0800

    LogMetadataFactory now extracts log text and generates a diff

--- src/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory/LogMetadataFactory.cs
+++ src/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory/LogMetadataFactory.cs
@@ -4,7 +4,12 @@ using System.Composition;
 using System.Collections.Generic;
 using LibGit2Sharp;
 using System.Linq;
+using System.IO;
+using System.Text;
 using Microsoft.Extensions.Logging;
+using LibGit2Sharp.Extensions;
+using System.Text.RegularExpressions;
+using Semver;
 
 namespace JeremyTCD.ContDeployer.Plugin.LogMetadataFactory
 {
@@ -13,79 +18,105 @@ namespace JeremyTCD.ContDeployer.Plugin.LogMetadataFactory
     {
         public override IDictionary<string, object> DefaultConfig { get; set; } = new Dictionary<string, object>
         {
-            { "fileName", "changelog" },
-            { "branch", "master" }
+            { "fileName", "changelog.md" },
+            { "branch", "master" },
+            // TODO include prerelease semver patterns
+            { "pattern", @"##[ \t]+(\d*\.\d*\.\d*)(.*?)(?=##[ \t]+\d*\.\d*\.\d*|$)"}
         };
 
         public override void Run(IDictionary<string, object> config, PipelineContext context, ILogger logger, LinkedList<PipelineStep> steps)
         {
+            logger.LogInformation("=== Running LogMetadataFactory ===");
+
             config = CombineConfigs(config, DefaultConfig);
-            string fileName = (string) config["filename"];
+            string fileName = (string)config["fileName"];
             string branch = (string)config["branch"];
+            string pattern = (string)config["pattern"];
 
             // TODO what if you're working another branch
-            List<Commit> commits = context.
-                Repository.
-                Commits.
-                QueryBy(new CommitFilter() { SortBy = CommitSortStrategies.Topological }).
-                ToList();
-
-            if (commits.Count == 0)
-            {
-                throw new Exception($"{nameof(LogMetadataFactory)}: Repository has no commits");
-            }
-
-            // Get trees for head and previous commit
-            Commit head = commits[0];
-            Tree newTree = head.Tree;
-            Commit previous = commits.Count == 1 ? null : commits[1];
-            Tree oldTree = previous?.Tree;
-
-            // Check whether changelog has been added or modified
-            bool changelogChanged = false;
-            if(commits.Count == 1)
-            {
-                changelogChanged = newTree.ToList().Any(treeEntry => treeEntry.Path == fileName);
-            }
-            else 
-            {
-                TreeChanges changes = context.Repository.Diff.Compare<TreeChanges>(oldTree, newTree, new string[] { fileName });
-                changelogChanged = changes.
-                    ToList().
-                    Any(treeEntryChanges => treeEntryChanges.Path == fileName && (treeEntryChanges.Status == ChangeKind.Modified || treeEntryChanges.Status == ChangeKind.Added));
-            }
-            if (!changelogChanged)
+            // Get changelog text
+            string headChangelogText = GetHeadChangelogText(context.Repository, fileName, logger);
+            if (headChangelogText == null)
             {
                 return;
             }
+            string previousChangelogText = GetPreviousChangelogText(context.Repository, fileName, logger);
+
+            // Check if changelog has changed
+            if (headChangelogText == previousChangelogText)
+            {
+                logger.LogInformation($"No changes to changelog: {fileName}");
+                return;
+            }
+
+            // Build changelog metadata
+            ChangelogMetadata headChangelogMetadata = GetChangelogMetadata(pattern, headChangelogText);
+            ChangelogMetadata previousChangelogMetadata = previousChangelogText != null ? 
+                GetChangelogMetadata(pattern, previousChangelogText): null;
+
+            // Diff changelog metadata
 
 
-            Console.WriteLine(newTree.Id);
-            // Check if changelog changed
-            //  - get changed files using git show: Invoke-Git show --pretty="" --name-only $commit
-            //  - check if changelog changed
-            //  - if no return
+            // if new version added, add taggenerator, githubreleasepublisher and wtv publisher
+            // if tag incremented, add taggenerator, githubreleasepublisher and wtv publisher
+            // if changes made, add githubreleasepublisher
 
-            // Checkout branch so current changelog is head version
 
-            // Checkout old changelog 
-            //  - Just checkout head^ changelog since this system does not allow
-            //    published stuff to be changed: Invoke -Git show $commit^:$file > 'changelogOld.md'
+            //Console.WriteLine(newTree.Id);
+        }
 
-            // Build log metadata
+        private ChangelogMetadata GetChangelogMetadata(string pattern, string changelogText)
+        {
+            ChangelogMetadata result = new ChangelogMetadata();
+            MatchCollection matches = Regex.Matches(changelogText, pattern, RegexOptions.Singleline);
 
-            // generate a simple diff, which versions have changed/been added
+            foreach(Match match in matches)
+            {
+                result.Versions.Add(new Version()
+                {
+                    Raw = match.Groups[0].Value,
+                    SemVersion = SemVersion.Parse(match.Groups[1].Value),
+                    Notes = match.Groups[2].Value
+                });
+            }
 
-            // Repo should come from context since its shared by all plugins, this way it can be injected
-            //using (Repository repo = new Repository("path/to/your/repo"))
-            //{
-            //    CheckoutOptions checkoutOptions = new CheckoutOptions()
-            //    {
-            //        CheckoutModifiers
-            //    }
-            //    Commands.Checkout(repo, "master");
+            return result;
+        }
 
-            //}
+        private string GetHeadChangelogText(IRepository repository, string fileName, ILogger logger)
+        {
+            Commit head = repository.Lookup<Commit>("HEAD");
+            if (head == null)
+            {
+                logger.LogInformation($"Repository has no commits");
+                return null;
+            }
+            GitObject headChangelogGitObject = head[fileName]?.Target;
+            if (headChangelogGitObject == null)
+            {
+                logger.LogInformation($"No file with name: {fileName}");
+                return null;
+            }
+
+            return ((Blob)headChangelogGitObject).ReadAsString();
+        }
+
+        private string GetPreviousChangelogText(IRepository repository, string fileName, ILogger logger)
+        {
+            Commit previous = repository.Lookup<Commit>("HEAD^");
+            if (previous == null)
+            {
+                logger.LogInformation($"First commit");
+                return null;
+            }
+            GitObject previousChangelogGitObject = previous[fileName]?.Target;
+            if (previous == null)
+            {
+                logger.LogInformation($"First commit for: {fileName}");
+                return null;
+            }
+
+            return ((Blob)previousChangelogGitObject).ReadAsString();
         }
     }
 }
