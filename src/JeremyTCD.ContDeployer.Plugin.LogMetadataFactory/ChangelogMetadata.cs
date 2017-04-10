2befefa6322798c231f859c662b1bea70f15ea54
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-10 14:37:29 +0800

    Completed ChangelogMetadata.Diff

--- src/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory/ChangelogMetadata.cs
+++ src/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory/ChangelogMetadata.cs
@@ -21,39 +21,55 @@ namespace JeremyTCD.ContDeployer.Plugin.ChangelogDeployer
         /// Creates a <see cref="ChangelogMetadataDiff"/> between this instance and another <see cref="ChangelogMetadata"/> instance.
         /// <see cref="ChangelogMetadata.Versions"/> of both instances must be in descending <see cref="Version.SemVersion"/> order.
         /// </summary>
-        /// <param name="otherChangelogMetadata"></param>
-        /// <returns></returns>
+        /// <param name="otherChangelogMetadata">
+        /// If null, all versions in this instance are considered to be newly added.
+        /// </param>
+        /// <returns>
+        /// <see cref="ChangelogMetadataDiff"/>
+        /// </returns>
         public ChangelogMetadataDiff Diff(ChangelogMetadata otherChangelogMetadata)
         {
-            // TODO: if other is null, add all versions to newly added 
-
             ChangelogMetadataDiff result = new ChangelogMetadataDiff();
-            int i = 0, j = 0;
 
-            while(i < Versions.Count && j < otherChangelogMetadata.Versions.Count)
+            if (otherChangelogMetadata == null)
             {
-                Version thisVersion = Versions[i];
-                Version otherVersion = otherChangelogMetadata.Versions[j];
-                int comparisonResult = thisVersion.SemVersion.CompareTo(otherVersion);
-                if ( comparisonResult > 1)
+                foreach (Version version in Versions)
                 {
-                    result.AddedVersions.Add(thisVersion);
-                    i++;
+                    result.AddedVersions.Add(version);
                 }
-                else if(comparisonResult < 1)
-                {
-                    result.RemovedVersions.Add(otherVersion);
-                    j++;
-                }
-                else
-                {
-                    if(thisVersion.Raw != otherVersion.Raw)
-                    {
-                        result.ModifiedVersions.Add(thisVersion);
-                    }
+            }
+            else
+            {
+                List<Version> otherVersions = otherChangelogMetadata.Versions;
+                int i = 0, j = 0;
 
-                    i++;
-                    j++;
+                while (i < Versions.Count || j < otherChangelogMetadata.Versions.Count)
+                {
+                    Version thisVersion = i < Versions.Count ? Versions[i] : null;
+                    Version otherVersion = j < otherVersions.Count ? otherVersions[j] : null;
+                    int comparisonResult = thisVersion == null ? -1 :
+                        otherVersion == null ? 1 :
+                        thisVersion.SemVersion.CompareTo(otherVersion.SemVersion);
+                    if (comparisonResult > 0)
+                    {
+                        result.AddedVersions.Add(thisVersion);
+                        i++;
+                    }
+                    else if (comparisonResult < 0)
+                    {
+                        result.RemovedVersions.Add(otherVersion);
+                        j++;
+                    }
+                    else
+                    {
+                        if (thisVersion.Raw != otherVersion.Raw)
+                        {
+                            result.ModifiedVersions.Add(thisVersion);
+                        }
+
+                        i++;
+                        j++;
+                    }
                 }
             }
 
--- test/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory.Tests/UnitTests/ChangelogMetadataTests.cs
+++ test/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory.Tests/UnitTests/ChangelogMetadataTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Semver;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -8,13 +9,121 @@ namespace JeremyTCD.ContDeployer.Plugin.ChangelogDeployer.Tests.UnitTests
     public class ChangelogMetadataTests
     {
         [Fact]
-        public void Compare_ComparesChangelogMetadataCorrectly()
+        public void Diff_AccountsForAddedVersions()
         {
             // Arrange
-            //ChangelogMetadata 
+            ChangelogMetadata oldMetadata = new ChangelogMetadata(
+                new List<Version>
+                {
+                    new Version(){SemVersion = SemVersion.Parse("1.1.0")},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0")}
+                });
+
+            ChangelogMetadata newMetadata = new ChangelogMetadata(
+                new List<Version>
+                {
+                    new Version(){SemVersion = SemVersion.Parse("2.1.1")}, // Add Version to start of list
+                    new Version(){SemVersion = SemVersion.Parse("1.1.0")},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.1")}, // Add Version to middle of list
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0")},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0-alpha")} // Add Version to end of list
+                });
 
             // Act
+            ChangelogMetadataDiff result = newMetadata.Diff(oldMetadata);
+
             // Assert 
+            Assert.NotNull(result.AddedVersions);
+            Assert.Equal(3, result.AddedVersions.Count);
+            Assert.Equal("2.1.1", result.AddedVersions[0].SemVersion.ToString());
+            Assert.Equal("1.0.1", result.AddedVersions[1].SemVersion.ToString());
+            Assert.Equal("1.0.0-alpha", result.AddedVersions[2].SemVersion.ToString());
+        }
+
+        [Fact]
+        public void Diff_AccountsForRemovedVersions()
+        {
+            // Arrange
+            ChangelogMetadata oldMetadata = new ChangelogMetadata(
+                new List<Version>
+                {
+                    new Version(){SemVersion = SemVersion.Parse("2.1.1")}, // Remove Version from start of list
+                    new Version(){SemVersion = SemVersion.Parse("1.1.0")},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.1")}, // Remove Version from middle of list
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0")},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0-alpha")} // Remove Version from end of list
+                });
+
+            ChangelogMetadata newMetadata = new ChangelogMetadata(
+                new List<Version>
+                {
+                    new Version(){SemVersion = SemVersion.Parse("1.1.0")},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0")}
+                });
+
+            // Act
+            ChangelogMetadataDiff result = newMetadata.Diff(oldMetadata);
+
+            // Assert 
+            Assert.NotNull(result.RemovedVersions);
+            Assert.Equal(3, result.RemovedVersions.Count);
+            Assert.Equal("2.1.1", result.RemovedVersions[0].SemVersion.ToString());
+            Assert.Equal("1.0.1", result.RemovedVersions[1].SemVersion.ToString());
+            Assert.Equal("1.0.0-alpha", result.RemovedVersions[2].SemVersion.ToString());
+        }
+
+        [Fact]
+        public void Diff_AccountsForModifiedVersions()
+        {
+            // Arrange
+            ChangelogMetadata oldMetadata = new ChangelogMetadata(
+                new List<Version>
+                {
+                    new Version(){SemVersion = SemVersion.Parse("1.1.1"), Raw = "## 1.1.1\nOld"},
+                    new Version(){SemVersion = SemVersion.Parse("1.1.0"), Raw = "## 1.1.0\nOld"},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0"), Raw = "## 1.0.0\nOld"}
+                });
+
+            ChangelogMetadata newMetadata = new ChangelogMetadata(
+                new List<Version>
+                {
+                    new Version(){SemVersion = SemVersion.Parse("1.1.1"), Raw = "## 1.1.1\nNew"},
+                    new Version(){SemVersion = SemVersion.Parse("1.1.0"), Raw = "## 1.1.0\nNew"},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0"), Raw = "## 1.0.0\nNew"}
+                });
+
+            // Act
+            ChangelogMetadataDiff result = newMetadata.Diff(oldMetadata);
+
+            // Assert 
+            Assert.NotNull(result.ModifiedVersions);
+            Assert.Equal(3, result.ModifiedVersions.Count);
+            Assert.Equal("## 1.1.1\nNew", result.ModifiedVersions[0].Raw);
+            Assert.Equal("## 1.1.0\nNew", result.ModifiedVersions[1].Raw);
+            Assert.Equal("## 1.0.0\nNew", result.ModifiedVersions[2].Raw);
+        }
+
+        [Fact]
+        public void Diff_ConsidersAllVersionsAsAddedIfOtherIsNull()
+        {
+            // Arrange
+            ChangelogMetadata newMetadata = new ChangelogMetadata(
+                new List<Version>
+                {
+                    new Version(){SemVersion = SemVersion.Parse("1.1.1")},
+                    new Version(){SemVersion = SemVersion.Parse("1.1.0")},
+                    new Version(){SemVersion = SemVersion.Parse("1.0.0")}
+                });
+
+            // Act
+            ChangelogMetadataDiff result = newMetadata.Diff(null);
+
+            // Assert 
+            Assert.NotNull(result.AddedVersions);
+            Assert.Equal(3, result.AddedVersions.Count);
+            Assert.Equal("1.1.1", result.AddedVersions[0].SemVersion.ToString());
+            Assert.Equal("1.1.0", result.AddedVersions[1].SemVersion.ToString());
+            Assert.Equal("1.0.0", result.AddedVersions[2].SemVersion.ToString());
         }
     }
 }
