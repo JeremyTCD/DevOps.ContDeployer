08bad05d2c8c719d1210aca5ba0354a1edf9667b
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-10 15:06:49 +0800

    Added ChangelogDeployerOptions and TagGeneratorOptions

---
+++ src/JeremyTCD.ContDeployer.Plugin.LogMetadataFactory/ChangelogDeployerOptions.cs
@@ -0,0 +1,15 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JeremyTCD.ContDeployer.Plugin.ChangelogDeployer
+{
+    public class ChangelogDeployerOptions : IPluginOptions
+    {
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/JeremyTCD.ContDeployer.Plugin.TagGenerator/TagGeneratorOptions.cs
@@ -0,0 +1,20 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JeremyTCD.ContDeployer.Plugin.TagGenerator
+{
+    public class TagGeneratorOptions : IPluginOptions
+    {
+        public string TagName { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(TagName))
+            {
+                throw new Exception($"{nameof(TagGeneratorOptions)}: {nameof(TagName)} cannot be null or empty");
+            }
+        }
+    }
+}
