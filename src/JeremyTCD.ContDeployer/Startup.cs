f8890b4f613feb55687b29de42c37bf5133d60b9
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-10 20:54:33 +0800

    Startup now adds services for plugins and their options

--- src/JeremyTCD.ContDeployer/Startup.cs
+++ src/JeremyTCD.ContDeployer/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace JeremyTCD.ContDeployer
@@ -35,12 +37,25 @@ namespace JeremyTCD.ContDeployer
 
             services.
                 AddSingleton<Pipeline>();
-
             services.Configure<PipelineOptions>(pipelineOptions =>
             {
                 ConfigurationBinder.Bind(_configurationRoot.GetSection("Pipeline"), pipelineOptions);
                 pipelineOptions.Validate();
             });
+
+            // Get plugin assemblies 
+            AssemblyService assemblyService = new AssemblyService();
+            assemblyService.LoadAssembliesInDir(Path.Combine(Directory.GetCurrentDirectory(), "plugins"), true);
+            IEnumerable<Assembly> pluginAssemblies = assemblyService.GetReferencingAssemblies(typeof(IPlugin).GetTypeInfo().Assembly);
+
+            // Get plugin and plugin options types
+            IEnumerable<Type> types = assemblyService.GetAssignableTypes(pluginAssemblies, typeof(IPlugin)).
+                Concat(assemblyService.GetAssignableTypes(pluginAssemblies, typeof(IPluginOptions)));
+
+            foreach (Type type in types)
+            {
+                services.AddTransient(type);
+            }
         }
 
         public void Configure(ILoggerFactory loggerFactory)
