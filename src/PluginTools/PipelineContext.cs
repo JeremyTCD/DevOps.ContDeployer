37a1411ebf31d8d492261c98c933876474434bf9
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-13 20:54:23 +0800

    Added PipelineContext and PipelineStepContext
    - Eliminates the need for plugin constructors, makes adding pipeline wide tools and objects easier

---
+++ src/PluginTools/PipelineContext.cs
@@ -0,0 +1,13 @@
+﻿using LibGit2Sharp;
+using System.Collections.Generic;
+
+namespace JeremyTCD.ContDeployer.PluginTools
+{
+    public class PipelineContext
+    {
+        public IProcessManager ProcessManager { get; set; }
+        public IRepository Repository { get; set; }
+        public Dictionary<string, object> SharedData { get; set; }
+        public LinkedList<PipelineStep> PipelineSteps { get; set; }
+    }
+}
---
+++ src/PluginTools/PipelineStepContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace JeremyTCD.ContDeployer.PluginTools
+{
+    public class PipelineStepContext
+    {
+        public IPluginOptions Options { get; set; }
+        public ILogger Logger { get; set; }
+    }
+}
