13827842eb6c21ed8bb75257a7deb082abc2fed0
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-13 12:44:09 +0800

    Added TagGenerator.Tests project

---
+++ test/TagGenerator/IntegrationTests/Shared.cs
@@ -0,0 +1,68 @@
+﻿using LibGit2Sharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.IO;
+using Xunit;
+
+namespace JeremyTCD.ContDeployer.Plugin.ChangelogDiffGenerator.IntegrationTests
+{
+    [CollectionDefinition(nameof(TagGeneratorCollection))]
+    public class TagGeneratorCollection : ICollectionFixture<TagGeneratorFixture>
+    {
+    }
+
+    public class TagGeneratorFixture : IDisposable
+    {
+        public string TempDir { get; }
+        public string TempPluginsDir { get; }
+        public string TempGitDir { get; }
+        public JsonSerializerSettings SerializerSettings { get; }
+        public Repository Repository { get; set; }
+        public Signature Signature { get; }
+
+        public TagGeneratorFixture()
+        {
+            TempDir = Path.Combine(Path.GetTempPath(), $"{nameof(ChangelogDiffGenerator)}Temp");
+            TempPluginsDir = Path.Combine(TempDir, "plugins");
+            TempGitDir = Path.Combine(TempDir, ".git");
+            SerializerSettings = new JsonSerializerSettings();
+            SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            Signature = new Signature(new Identity("TestName", "TestEmail"), DateTime.Now);
+        }
+
+        // Deletes entire temp directory, recreates it and inits git repository
+        public void ResetTempDir()
+        {
+            Directory.SetCurrentDirectory("\\");
+
+            if (Directory.Exists(TempDir))
+            {
+                if (Directory.Exists(TempGitDir))
+                {
+                    string[] gitFiles = Directory.GetFiles(TempGitDir, "*", SearchOption.AllDirectories);
+                    foreach (string file in gitFiles)
+                    {
+                        File.SetAttributes(file, FileAttributes.Normal);
+                    }
+                }
+
+                Directory.Delete(TempDir, true);
+            }
+            Directory.CreateDirectory(TempDir);
+            Directory.CreateDirectory(TempPluginsDir);
+
+            Repository.Init(TempDir);
+
+            Repository = new Repository(TempDir);
+
+            Directory.SetCurrentDirectory(TempDir);
+        }
+
+        public void Dispose()
+        {
+            Repository.Dispose();
+        }
+    }
+}
+
---
+++ test/TagGenerator/IntegrationTests/TagGeneratorTests.cs
@@ -0,0 +1,32 @@
+﻿using LibGit2Sharp;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace JeremyTCD.ContDeployer.Plugin.ChangelogDiffGenerator.IntegrationTests
+{
+    [Collection(nameof(TagGeneratorCollection))]
+    public class TagGeneratorTests
+    {
+        private string _tempDir { get; }
+        private string _tempPluginsDir { get; }
+        private JsonSerializerSettings _serializerSettings { get; }
+        private Repository _repository { get; }
+        private Signature _signature { get; }
+
+        public TagGeneratorTests(TagGeneratorFixture fixture)
+        {
+            fixture.ResetTempDir();
+            _tempDir = fixture.TempDir;
+            _tempPluginsDir = fixture.TempPluginsDir;
+            _serializerSettings = fixture.SerializerSettings;
+            _repository = fixture.Repository;
+            _signature = fixture.Signature;
+        }
+
+        [Fact]
+        public void Run()
+        {
+
+        }
+    }
+}
