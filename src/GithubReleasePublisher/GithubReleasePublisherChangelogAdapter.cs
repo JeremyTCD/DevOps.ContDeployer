ea607d61ae401c9049f7388d9ecfa4887b9399ec
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-18 21:37:09 +0800

    GithubReleasePublisherChangelogAdapter now retrieves all releases

--- src/GithubReleasePublisher/GithubReleasePublisherChangelogAdapter.cs
+++ src/GithubReleasePublisher/GithubReleasePublisherChangelogAdapter.cs
@@ -1,11 +1,18 @@
 ﻿using JeremyTCD.ContDeployer.Plugin.ChangelogGenerator;
 using JeremyTCD.ContDeployer.PluginTools;
+using Octokit;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 
 namespace JeremyTCD.ContDeployer.Plugin.GithubReleasePublisher
 {
     public class GithubReleasePublisherChangelogAdapter : IPlugin
     {
+        private PipelineContext _pipelineContext { get; set; }
+        private GithubReleasePublisherChangelogAdapterOptions _options { get; set; }
+
         /// <summary>
         /// Tags head
         /// </summary>
@@ -13,26 +20,38 @@ namespace JeremyTCD.ContDeployer.Plugin.GithubReleasePublisher
         /// <param name="steps"></param>
         public void Run(PipelineContext pipelineContext, StepContext stepContext)
         {
-            pipelineContext.SharedData.TryGetValue(nameof(Changelog), out object diffObject);
-            Changelog diff = diffObject as Changelog;
-            if (diff == null)
+            _options = stepContext.Options as GithubReleasePublisherChangelogAdapterOptions;
+
+            if (_options == null)
+            {
+                throw new InvalidOperationException($"{nameof(GithubReleasePublisherChangelogAdapterOptions)} required");
+            }
+
+            _pipelineContext = pipelineContext;
+
+            pipelineContext.SharedData.TryGetValue(nameof(Changelog), out object changelogObject);
+            Changelog changelog = changelogObject as Changelog;
+            if (changelog == null)
             {
                 throw new InvalidOperationException($"No {nameof(Changelog)} in {nameof(pipelineContext.SharedData)}");
             }
 
-            foreach(ChangelogGenerator.Version version in diff.AddedVersions)
-            {
-                // Add GithubReleasePublisher to create release
-            }
+            List<ChangelogGenerator.Version> versions = changelog.Versions.ToList();
 
-            // TODO version must include tag field
-            //   - tag field must be used to create tag on head
-            //      - dont forget to push to origin
-            //   - tag must be provided as tag_name 
-            foreach(ChangelogGenerator.Version version in diff.ModifiedVersions)
-            {
-                // Add GithubReleasePublisher to edit release
-            }
+            List<Release> releases = GetGithubReleases();
+
+            // iterate through versions, ensure that each has a corresponding release with the same notes
+        }
+
+        public List<Release> GetGithubReleases()
+        {
+            GitHubClient client = new GitHubClient(new ProductHeaderValue(nameof(ContDeployer)));
+            Credentials credentials = new Credentials(_options.Token);
+            client.Credentials = credentials;
+
+            List<Release> releases = client.Repository.Release.GetAll(_options.Owner, _options.Repository).Result.ToList();
+
+            return releases;
         }
     }
 }
---
+++ src/GithubReleasePublisher/GithubReleasePublisherChangelogAdapterOptions.cs
@@ -0,0 +1,30 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using System;
+
+namespace JeremyTCD.ContDeployer.Plugin.GithubReleasePublisher
+{
+    public class GithubReleasePublisherChangelogAdapterOptions : IPluginOptions
+    {
+        public string Token { get; set; }
+        public string Owner { get; set; }
+        public string Repository { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(Token))
+            {
+                throw new Exception($"{nameof(Token)} cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(Owner))
+            {
+                throw new Exception($"{nameof(Owner)} cannot be null or empty");
+            }
+
+            if (string.IsNullOrEmpty(Repository))
+            {
+                throw new Exception($"{nameof(Repository)} cannot be null or empty");
+            }
+        }
+    }
+}
