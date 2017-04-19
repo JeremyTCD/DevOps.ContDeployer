f3ec18d8dee3fe54e9d2d35d328d8951b21153b8
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-19 20:56:03 +0800

    Added tests for GithubReleasesChangelogAdapter

---
+++ test/GithubReleases.Tests/IntegrationTests/GithubReleasesChangelogAdapterTests.cs
@@ -0,0 +1,53 @@
+﻿using JeremyTCD.ContDeployer.Plugin.GithubReleases;
+using JeremyTCD.ContDeployer.PluginTools;
+using LibGit2Sharp;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using System;
+using Xunit;
+
+namespace JeremyTCD.ContDeployer.Plugin.GithubReleases.IntegrationTests
+{
+    [Collection(nameof(GithubReleasesCollection))]
+    public class GithubReleasesChangelogAdapterTests
+    {
+        private JsonSerializerSettings _serializerSettings { get; }
+        private Signature _signature { get; }
+        private GithubReleasesFixture _fixture { get; }
+
+        public GithubReleasesChangelogAdapterTests(GithubReleasesFixture fixture)
+        {
+            fixture.ResetTempDir();
+            _fixture = fixture;
+            _serializerSettings = fixture.SerializerSettings;
+            _signature = fixture.Signature;
+        }
+
+        [Fact]
+        public void Run_ThrowsExceptionIfOptionsIsNull()
+        {
+            // Arrange
+            StepContext stepContext = _fixture.CreateStepContext(null, null);
+
+            GithubReleasesChangelogAdapter adapter = new GithubReleasesChangelogAdapter();
+
+            // Act and Assert
+            Assert.Throws<InvalidOperationException>(() => adapter.Run(null, stepContext));
+        }
+
+        [Fact]
+        public void Run_ThrowsExceptionIfSharedDataDoesNotContainChangelog()
+        {
+            // Arrange
+            PipelineContext pipelineContext = _fixture.CreatePipelineContext();
+            StepContext stepContext = _fixture.
+                            CreateStepContext(null, new GithubReleasesOptions());
+
+            GithubReleasesChangelogAdapter adapter = new GithubReleasesChangelogAdapter();
+
+            // Act and Assert
+            Assert.Throws<InvalidOperationException>(() => adapter.Run(null, stepContext));
+        }
+    }
+}
---
+++ test/GithubReleases.Tests/IntegrationTests/Shared.cs
@@ -0,0 +1,100 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using LibGit2Sharp;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Xunit;
+
+namespace JeremyTCD.ContDeployer.Plugin.GithubReleases.IntegrationTests
+{
+    [CollectionDefinition(nameof(GithubReleasesCollection))]
+    public class GithubReleasesCollection : ICollectionFixture<GithubReleasesFixture>
+    {
+    }
+
+    public class GithubReleasesFixture : IDisposable
+    {
+        public string TempDir { get; }
+        public string TempPluginsDir { get; }
+        public string TempGitDir { get; }
+        public JsonSerializerSettings SerializerSettings { get; }
+        public Repository Repository { get; set; }
+        public Signature Signature { get; }
+
+        public GithubReleasesFixture()
+        {
+            TempDir = Path.Combine(Path.GetTempPath(), $"{nameof(GithubReleases)}Temp");
+            TempPluginsDir = Path.Combine(TempDir, "plugins");
+            TempGitDir = Path.Combine(TempDir, ".git");
+            SerializerSettings = new JsonSerializerSettings();
+            SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            Signature = new Signature(new Identity("TestName", "TestEmail"), DateTime.Now);
+        }
+
+        // Deletes entire temp directory, recreates it and inits git repository
+        public void ResetTempDir()
+        {
+            Directory.SetCurrentDirectory("\\");
+
+            if (Directory.Exists(TempDir))
+            {
+                if (Directory.Exists(TempGitDir))
+                {
+                    string[] gitFiles = Directory.GetFiles(TempGitDir, "*", SearchOption.AllDirectories);
+                    foreach (string file in gitFiles)
+                    {
+                        File.SetAttributes(file, FileAttributes.Normal);
+                    }
+                }
+
+                Directory.Delete(TempDir, true);
+            }
+            Directory.CreateDirectory(TempDir);
+            Directory.CreateDirectory(TempPluginsDir);
+
+            Repository.Init(TempDir);
+
+            Repository = new Repository(TempDir);
+
+            Directory.SetCurrentDirectory(TempDir);
+        }
+
+        public PipelineContext CreatePipelineContext(SharedOptions sharedOptions = null)
+        {
+            Dictionary<string, object> sharedData = new Dictionary<string, object>();
+            LinkedList<Step> steps = new LinkedList<Step>();
+            Mock<ILogger<ProcessManager>> mockLogger = new Mock<ILogger<ProcessManager>>();
+            Mock<IOptions<SharedOptions>> mockOptions = new Mock<IOptions<SharedOptions>>();
+            mockOptions.Setup(o => o.Value).Returns(sharedOptions == null ? new SharedOptions() : sharedOptions);
+            ProcessManager processManager = new ProcessManager(mockLogger.Object, mockOptions.Object);
+
+            return new PipelineContext
+            {
+                Repository = Repository,
+                ProcessManager = processManager,
+                SharedData = sharedData,
+                Steps = steps
+            };
+        }
+
+        public StepContext CreateStepContext(ILogger logger, IPluginOptions options)
+        {
+            return new StepContext
+            {
+                Logger = logger,
+                Options = options
+            };
+        }
+
+        public void Dispose()
+        {
+            Repository.Dispose();
+        }
+    }
+}
+
