76b956bbe6ba0319552acaa8cce1a71dda45db1a
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-20 21:45:26 +0800

    Added IPluginStartup to PluginTools
    - Each plugin can register/override services for its own consumption
    - Necessary for testing plugins and for making base architecture extensible

---
+++ src/PluginTools/IPluginStartup.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JeremyTCD.ContDeployer.PluginTools
+{
+    public interface IPluginStartup
+    {
+        void ConfigureServices(IServiceCollection services);
+    }
+}
