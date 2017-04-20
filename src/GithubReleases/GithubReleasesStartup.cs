4676a5f59230783aaa8b7dfe6276620f8ac99653
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-20 21:45:45 +0800

    Added GithubReleasesStartup

---
+++ src/GithubReleases/GithubReleasesStartup.cs
@@ -0,0 +1,14 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using Microsoft.Extensions.DependencyInjection;
+using Octokit;
+
+namespace JeremyTCD.ContDeployer.Plugin.GithubReleases
+{
+    public class GithubReleasesStartup : IPluginStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGitHubClient>(provider => new GitHubClient(new ProductHeaderValue(nameof(ContDeployer))));
+        }
+    }
+}
