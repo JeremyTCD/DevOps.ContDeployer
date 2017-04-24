4a8de30c19c829ea8639a441c64d290a23cba77c
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-24 21:54:19 +0800

    Added GitHubReleasesTestHelpers, added NSubstitute to the project

---
+++ test/GitHubReleases.Tests/GitHubReleasesTestHelpers.cs
@@ -0,0 +1,41 @@
+﻿using NSubstitute;
+using Octokit;
+using System.Collections.Generic;
+
+namespace JeremyTCD.ContDeployer.Plugin.GitHubReleases.Tests
+{
+    public class GitHubReleasesTestHelpers
+    {
+        public static IGitHubClientFactory CreateGitHubClientFactory(string token, IGitHubClient gitHubClient)
+        {
+            IGitHubClientFactory mockGitHubClientFactory = Substitute.For<IGitHubClientFactory>();
+            mockGitHubClientFactory.CreateClient(token).Returns(gitHubClient);
+
+            return mockGitHubClientFactory;
+        }
+
+        public static IGitHubClient CreateGitHubClient(IRepositoriesClient repositoriesClient)
+        {
+            IGitHubClient mockGitHubClient = Substitute.For<IGitHubClient>();
+            mockGitHubClient.Repository.Returns(repositoriesClient);
+
+            return mockGitHubClient;
+        }
+
+        public static IRepositoriesClient CreateRepositoriesClient(IReleasesClient releaseClient)
+        {
+            IRepositoriesClient mockRepositoriesClient = Substitute.For<IRepositoriesClient>();
+            mockRepositoriesClient.Release.Returns(releaseClient);
+
+            return mockRepositoriesClient;
+        }
+
+        public static IReleasesClient CreateReleaseClient(string owner, string repository, List<Release> releases)
+        {
+            IReleasesClient mockReleaseClient = Substitute.For<IReleasesClient>();
+            mockReleaseClient.GetAll(owner, repository).Returns(releases);
+
+            return mockReleaseClient;
+        }
+    }
+}
