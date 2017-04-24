4956614346acced0ab665258f010b5a81a6b7729
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-24 21:06:45 +0800

    Added ChangelogGeneratorTestHelpers, project now references NSubstitute

---
+++ test/ChangelogGenerator/ChangelogGeneratorTestHelpers.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+
+namespace JeremyTCD.ContDeployer.Plugin.ChangelogGenerator.Tests
+{
+    public class ChangelogGeneratorTestHelpers
+    {
+        public static IChangelogFactory CreateChangelogFactory(IChangelog changelog, string pattern, string text)
+        {
+            IChangelogFactory changelogFactory = Substitute.For<IChangelogFactory>();
+            changelogFactory.Build(pattern, text).Returns(changelog);
+
+            return changelogFactory;
+        }
+
+        public static IChangelog CreateChangelog()
+        {
+            return Substitute.For<IChangelog>();
+        }
+    }
+}
