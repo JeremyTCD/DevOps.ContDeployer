c9afb2ac89e20e2440bb5e629cd0fb7772e95f68
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-28 22:29:09 +0800

    Added Chocolatey plugin

---
+++ src/Chocolatey/ChocolateyChangelogAdapter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JeremyTCD.ContDeployer.Plugin.Chocolatey
+{
+    public class ChocolateyChangelogAdapter
+    {
+    }
+}
