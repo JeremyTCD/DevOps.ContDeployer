f0d770883700e48833f41c5c746ff499168930ed
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-28 22:29:21 +0800

    Added Nuget plugin

---
+++ src/Nuget/NugetChangelogAdapter.cs
@@ -0,0 +1,57 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using NuGet.Commands;
+using NuGet.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace JeremyTCD.ContDeployer.Plugin.Nuget
+{
+    public class NugetChangelogAdapter : PluginBase
+    {
+        private IListCommandRunner _listCommandRunner { get; }
+        private NugetChangelogAdapterOptions _options { get; }
+
+
+        public NugetChangelogAdapter(IPipelineContext pipelineContext, IStepContext stepContext, IListCommandRunner listCommandRunner) :
+            base(pipelineContext, stepContext)
+        {
+            _options = stepContext.Options as NugetChangelogAdapterOptions;
+
+            if (_options == null)
+            {
+                throw new InvalidOperationException($"{nameof(NugetChangelogAdapterOptions)} required");
+            }
+
+            _listCommandRunner = listCommandRunner;
+        }
+
+        public override void Run()
+        {
+        }
+
+        private void GetPackageVersions()
+        {
+            IList<PackageSource> packageSources = _options.
+                Sources.
+                Select(s => new PackageSource(s)).
+                ToList();
+
+            // TODO don't even need ListCommandRunenr since it just prints, we want the actual values
+            ListArgs args = new ListArgs(new List<string>{_options.PackageName}, 
+                packageSources, 
+                NullSettings.Instance, // Not used by ListCommandRunner
+                null, // TODO nuget has its own ilogger class, figure out how to use standard logger
+                null, // TODO nuget has its own ilogger class, figure out how to use standard logger
+                true, 
+                "No packaged", // TODO move these to strings.resx, copy actual strings from nuget.client
+                "License url: {0}", 
+                "Not supported",
+                true, true, true, CancellationToken.None);
+
+            _
+        }
+    }
+}
---
+++ src/Nuget/NugetChangelogAdapterOptions.cs
@@ -0,0 +1,27 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JeremyTCD.ContDeployer.Plugin.Nuget
+{
+    public class NugetChangelogAdapterOptions : IPluginOptions
+    {
+        public virtual IList<string> Sources { get; set; } = new List<string>();
+        public virtual string PackageName { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrEmpty(PackageName))
+            {
+                throw new Exception($"{nameof(PackageName)} cannot be null or empty");
+            }
+
+            if(Sources.Count == 0)
+            {
+                throw new Exception($"{nameof(Sources)} cannot be empty");
+            }
+            // TODO
+        }
+    }
+}
