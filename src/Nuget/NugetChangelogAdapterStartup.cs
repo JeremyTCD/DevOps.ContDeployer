3e310e50e21bc93b6893909fe11e95a3b51e112e
JeremyTCD <jeremytanchongdao@gmail.com>
2017-04-29 21:06:57 +0800

    Added NugetChangelogAdapterStartup.cs

---
+++ src/Nuget/NugetChangelogAdapterStartup.cs
@@ -0,0 +1,15 @@
+﻿using JeremyTCD.ContDeployer.PluginTools;
+using Microsoft.Extensions.DependencyInjection;
+using NuGet.Common;
+
+namespace JeremyTCD.ContDeployer.Plugin.Nuget
+{
+    public class NugetChangelogAdapterStartup : IPluginStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ILogger, NugetLogger>();
+            services.AddSingleton<INugetClient, NugetClient>();
+        }
+    }
+}
