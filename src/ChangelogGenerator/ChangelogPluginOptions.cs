a8e8036f4ed26d55c695c2649a61991392cf739f
JeremyTCD <jeremytanchongdao@gmail.com>
2017-05-17 22:32:09 +0800

    Added minor comments to ChangelogPluginOptions

--- src/ChangelogGenerator/ChangelogPluginOptions.cs
+++ src/ChangelogGenerator/ChangelogPluginOptions.cs
@@ -5,6 +5,10 @@ namespace JeremyTCD.ContDeployer.Plugin.Changelog
     public class ChangelogPluginOptions : IPluginOptions
     {
         public virtual string FileName { get; set; } = "changelog.md";
+
+        /// <summary>
+        /// Branch to checkout. Defaults to master if unspecified.
+        /// </summary>
         public virtual string Branch { get; set; } = "master";
         // http://semver.org/
         public virtual string Pattern { get; set; } = @"##[ \t]+(\d*\.\d*\.\d*(?:-[a-zA-Z0-9\.-]+)?(?:\+[a-zA-Z0-9\.-]+)?)(.*?)(?=##|$)";
