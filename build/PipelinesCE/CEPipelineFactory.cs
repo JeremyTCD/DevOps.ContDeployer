8afd14c1b128e2963f0c71fc992fdad7aafbeea5
JeremyTCD <jeremytanchongdao@gmail.com>
2017-05-17 22:45:44 +0800

    Added project PipelinesCE

---
+++ build/PipelinesCE/CEPipelineFactory.cs
@@ -0,0 +1,33 @@
+﻿using JeremyTCD.ContDeployer;
+using JeremyTCD.ContDeployer.Plugin.Changelog;
+using JeremyTCD.ContDeployer.Plugin.Git;
+using JeremyTCD.ContDeployer.Plugin.MSBuild;
+using JeremyTCD.ContDeployer.PluginTools;
+using System.Collections.Generic;
+
+namespace PipelinesCE
+{
+    public class CEPipelineFactory : IPipelineFactory
+    {
+        public IEnumerable<IStep> CreatePipeline()
+        {
+            return new IStep[]{
+                // TODO is there some way to verify that the correct options type is provided?
+                new Step<ChangelogPlugin>(new ChangelogPluginOptions
+                {
+                        Branch = "master",
+                        FileName = "changelog.md",
+                        Pattern = ""
+                }),
+                new Step<MSBuildPlugin>(new MSBuildPluginOptions
+                {
+                    
+                }),
+                new Step<GitPlugin>(new GitPluginOptions
+                {
+
+                })
+            };
+        }
+    }
+}
