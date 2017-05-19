44392882b0ee1ad19180947722f16a4195e08973
JeremyTCD <jeremytanchongdao@gmail.com>
2017-05-19 21:44:43 +0800

    Added comments for IStepContextFactory

--- src/PluginTools/IStepContextFactory.cs
+++ src/PluginTools/IStepContextFactory.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 namespace JeremyTCD.ContDeployer.PluginTools
 {
+    /// <summary>
+    /// Factory for <see cref="IStepContext"/> instances. This interface decouples <see cref="IStepContext"/> from <see cref="StepContext"/>.
+    /// Uses multiple functions to specify objects required to build instead of multiple parameters on the <see cref="Build"/> function so that 
+    /// objects can be added or removed without changing <see cref="Build"/>. 
+    /// </summary>
     public interface IStepContextFactory
     {
         IStepContextFactory AddRemainingSteps(IEnumerable<IStep> steps);
