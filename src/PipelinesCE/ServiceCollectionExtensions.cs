ad83cc3c35bd027c2c17e41e4ee9895879b79afc
JeremyTCD <jeremytanchongdao@gmail.com>
2017-05-24 18:45:25 +0800

    AddPipelinesCE no longer takes ConfigurationRoot as an argument

--- src/PipelinesCE/ServiceCollectionExtensions.cs
+++ src/PipelinesCE/ServiceCollectionExtensions.cs
@@ -15,7 +15,7 @@ namespace JeremyTCD.PipelinesCE
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddPipelinesCE(this IContainer main, IConfigurationRoot configurationRoot)
+        public static void AddPipelinesCE(this IContainer main)
         {
             ServiceCollection services = new ServiceCollection();
             AssemblyService assemblyService = new AssemblyService();
@@ -28,37 +28,37 @@ namespace JeremyTCD.PipelinesCE
                 AddSingleton(main);
 
             services.
+                AddSingleton<PipelinesCE>().
                 AddSingleton<IProcessManager, ProcessManager>().
                 AddSingleton<IPluginFactory, PluginFactory>().
                 AddSingleton<IPipeline, Pipeline>().
-                AddSingleton<IPipelineContext, PipelineContext>().
-                Configure<SharedOptions>(configurationRoot.GetSection("shared")); // TODO not using json configuration anymore
+                AddSingleton<IPipelineContext, PipelineContext>();
 
-            // Load assemblies in plugins directory
-            assemblyService.LoadAssembliesInDir(Path.Combine(Directory.GetCurrentDirectory(), "plugins"), true);
-            IEnumerable<Assembly> pluginAssemblies = assemblyService.GetReferencingAssemblies(typeof(IPlugin).GetTypeInfo().Assembly);
+            //// Load assemblies in plugins directory
+            //assemblyService.LoadAssembliesInDir(Path.Combine(Directory.GetCurrentDirectory(), "plugins"), true);
+            //IEnumerable<Assembly> pluginAssemblies = assemblyService.GetReferencingAssemblies(typeof(IPlugin).GetTypeInfo().Assembly);
 
-            // Plugins
-            List<Type> pluginTypes = assemblyService.GetAssignableTypes(pluginAssemblies, typeof(IPlugin)).ToList();
-            foreach (Type pluginType in pluginTypes)
-            {
-                services.AddTransient(pluginType);
-            }
+            //// Plugins
+            //List<Type> pluginTypes = assemblyService.GetAssignableTypes(pluginAssemblies, typeof(IPlugin)).ToList();
+            //foreach (Type pluginType in pluginTypes)
+            //{
+            //    services.AddTransient(pluginType);
+            //}
 
             // Plugin containers
-            List<Type> pluginStartupTypes = assemblyService.GetAssignableTypes(pluginAssemblies, typeof(IPluginStartup)).ToList();
-            Dictionary<string, IContainer> pluginContainers = new Dictionary<string, IContainer>();
-            foreach (Type pluginStartupType in pluginStartupTypes)
-            {
-                IPluginStartup pluginStartup = (IPluginStartup)Activator.CreateInstance(pluginStartupType);
-                ServiceCollection pluginServices = new ServiceCollection();
-                pluginStartup.ConfigureServices(pluginServices);
+            //List<Type> pluginStartupTypes = assemblyService.GetAssignableTypes(pluginAssemblies, typeof(IPluginStartup)).ToList();
+            //Dictionary<string, IContainer> pluginContainers = new Dictionary<string, IContainer>();
+            //foreach (Type pluginStartupType in pluginStartupTypes)
+            //{
+            //    IPluginStartup pluginStartup = (IPluginStartup)Activator.CreateInstance(pluginStartupType);
+            //    ServiceCollection pluginServices = new ServiceCollection();
+            //    pluginStartup.ConfigureServices(pluginServices);
 
-                IContainer child = main.CreateChildContainer();
-                child.Configure(config => config.Populate(pluginServices));
-                pluginContainers.Add(pluginStartupType.Name.Replace("Startup", ""), child);
-            }
-            services.AddSingleton<IDictionary<string, IContainer>>(pluginContainers);
+            //    IContainer child = main.CreateChildContainer();
+            //    child.Configure(config => config.Populate(pluginServices));
+            //    pluginContainers.Add(pluginStartupType.Name.Replace("Startup", ""), child);
+            //}
+            //services.AddSingleton<IDictionary<string, IContainer>>(pluginContainers);
 
             main.Populate(services);
         }
