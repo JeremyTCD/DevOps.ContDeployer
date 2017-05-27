dd8851f8b38e8a07df8ec6c78225a999e346986c
JeremyTCD <jeremytanchongdao@gmail.com>
2017-05-27 21:26:18 +0800

    Added tests project for CommandLineApp

---
+++ test/CommandLineApp/IntegrationTests/DefaultCommandIntegrationTests.cs
@@ -0,0 +1,26 @@
+﻿using StructureMap;
+using Xunit;
+
+namespace JeremyTCD.PipelinesCE.CommandLineApp.Tests.IntegrationTests
+{
+    [Collection(nameof(CommandLineAppCollection))]
+    public class PipelinesCEIntegrationTests
+    {
+        private string _tempDir { get; }
+        private IContainer _container { get; }
+
+        public PipelinesCEIntegrationTests(CommandLineAppFixture fixture)
+        {
+            fixture.ResetTempDir();
+            _container = fixture.GetContainer();
+            _tempDir = fixture.TempDir;
+        }
+
+        [Fact]
+        public void Start_ThrowsExceptionIfPipelinesCEProjectFileDoesNotExist()
+        {
+
+        }
+
+    }
+}
---
+++ test/CommandLineApp/IntegrationTests/Shared.cs
@@ -0,0 +1,50 @@
+﻿using JeremyTCD.PipelinesCE.CommandLineApp;
+using Microsoft.Extensions.Logging;
+using StructureMap;
+using System.IO;
+using Xunit;
+
+namespace JeremyTCD.PipelinesCE.CommandLineApp.Tests.IntegrationTests
+{
+    [CollectionDefinition(nameof(CommandLineAppCollection))]
+    public class CommandLineAppCollection : ICollectionFixture<CommandLineAppFixture>
+    {
+    }
+
+    public class CommandLineAppFixture 
+    {
+        public string TempDir { get; }
+
+        public CommandLineAppFixture()
+        {
+            TempDir = Path.Combine(Path.GetTempPath(), $"{nameof(CommandLineApp)}Temp");
+        }
+
+        public IContainer GetContainer()
+        {
+            Startup startup = new Startup();
+            IContainer main = new Container();
+            startup.ConfigureServices(main);
+
+            // TODO this should be configured by PipelinesCE
+            //main.GetInstance<ILoggerFactory>().AddDebug();
+
+            return main;
+        }
+
+        // Deletes entire temp directory, recreates it and inits git repository
+        public void ResetTempDir()
+        {
+            Directory.SetCurrentDirectory("\\");
+
+            if (Directory.Exists(TempDir))
+            {
+                Directory.Delete(TempDir, true);
+            }
+            Directory.CreateDirectory(TempDir);
+
+            Directory.SetCurrentDirectory(TempDir);
+        }
+    }
+}
+
