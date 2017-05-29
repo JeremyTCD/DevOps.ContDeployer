d6190216fa20af480dd303b35982e17a5835192a
JeremyTCD <jeremytanchongdao@gmail.com>
2017-05-29 20:27:48 +0800

    Made changes to RunCommand

--- src/CommandLineApp/RunCommand.cs
+++ src/CommandLineApp/RunCommand.cs
@@ -1,4 +1,5 @@
-﻿using JeremyTCD.PipelinesCE.PluginTools;
+﻿using JeremyTCD.DotNetCore.Utils;
+using JeremyTCD.PipelinesCE.PluginTools;
 using Microsoft.Extensions.CommandLineUtils;
 using System;
 
@@ -7,35 +8,41 @@ namespace JeremyTCD.PipelinesCE.CommandLineApp
     // TODO each command can actually be tested individually
     public class RunCommand : CommandLineApplication
     {
-        private CommandOption _project { get; }
-        private CommandOption _pipeline { get; }
-        private CommandOption _dryRun { get; }
+        private CommandOption _project { get; set; }
+        private CommandOption _pipeline { get; set; }
+        private CommandOption _dryRun { get; set; }
+
         private PipelinesCE _pipelinesCE { get; }
+        private ICommandLineUtilsService _cluService { get; }
 
-        public RunCommand(PipelinesCE pipelinesCE)
+        public RunCommand(PipelinesCE pipelinesCE, ICommandLineUtilsService cluService)
         {
-            Description = "Runs a pipeline. Usage: \"pipelinesce run [-pj <project>] [-pl <pipeline>]\"";
-
-            _project = Option("-pj | --project",
-                "Project file (.csproj) of project used to define pipeline(s). If unspecified, searches recursively for PipelinesCE.csproj in the current directory.",
-                CommandOptionType.SingleValue);
-            _pipeline = Option("-pl | --pipeline",
-                "Name of pipeline to run. By convention, a pipeline's name is its factories name less \"PipelineFactory\". For example, the name of the pipeline produced by" +
-                "\"MainPipelineFactory\" is \"Main\". If unspecified and only one pipeline has been defined, runs said pipeline.",
-                CommandOptionType.SingleValue);
-            _dryRun = Option("-d | --dryrun",
-                "If included, runs pipeline in dry run mode.",
-                CommandOptionType.NoValue);
-
             _pipelinesCE = pipelinesCE;
-            HelpOption("-? | -h | --help");
+            _cluService = cluService;
 
+            Description = Strings.RunCommandDescription;
+            Name = nameof(RunCommand).Replace("Command", "").ToLowerInvariant();
+            SetupOptions();
             OnExecute((Func<int>)Run);
+        }
+
+        private void SetupOptions()
+        {
+            _project = Option(_cluService.CreateOptionTemplate(Strings.ProjectOptionShortName, Strings.ProjectOptionLongName),
+                Strings.ProjectOptionDescription,
+                CommandOptionType.SingleValue);
+            _pipeline = Option(_cluService.CreateOptionTemplate(Strings.PipelineOptionShortName, Strings.PipelineOptionLongName),
+                Strings.ProjectOptionDescription,
+                CommandOptionType.SingleValue);
+            _dryRun = Option(_cluService.CreateOptionTemplate(Strings.DryRunOptionShortName, Strings.DryRunOptionLongName),
+                Strings.DryRunDescription,
+                CommandOptionType.NoValue);
+            HelpOption(_cluService.CreateOptionTemplate(Strings.HelpOptionShortName, Strings.HelpOptionLongName, Strings.HelpOptionSymbolName));
         }
 
         private int Run()
         {
-            DefaultCommand defaultCommand = (DefaultCommand)Parent;
+            RootCommand defaultCommand = (RootCommand)Parent;
 
             PipelineOptions pipelineOptions = new PipelineOptions
             {
