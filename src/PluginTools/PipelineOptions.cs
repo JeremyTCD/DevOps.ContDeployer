7c39f7b3f67b6b87fd866a647370894f5cba1ced
JeremyTCD <jeremytanchongdao@gmail.com>
2017-05-31 21:21:17 +0800

    Added default Project value for PipelineOptions

--- src/PluginTools/PipelineOptions.cs
+++ src/PluginTools/PipelineOptions.cs
@@ -1,10 +1,10 @@
 ﻿namespace JeremyTCD.PipelinesCE.PluginTools
 {
     public class PipelineOptions
-    {
+    { 
         public virtual bool Verbose { get; set; }
-        public virtual string Project { get; set; }
+        public virtual string Project { get; set; } = "PipelinesCE.csproj";
         public virtual string Pipeline { get; set; }
-        public virtual bool DryRun { get; set; } 
+        public virtual bool DryRun { get; set; }
     }
 }
