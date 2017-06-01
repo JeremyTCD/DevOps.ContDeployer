cd519cf1a4ded8cfb26a8267fd4859be1eae7884
JeremyTCD <jeremytanchongdao@gmail.com>
2017-06-01 21:42:22 +0800

    Added a test for PipelineOptions.Combine

---
+++ test/PluginTools/UnitTests/PipelineOptionsUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace JeremyTCD.PipelinesCE.PluginTools.Tests.UnitTests
+{
+    public class PipelineOptionsUnitTests
+    {
+        [Fact]
+        public void Combine_CombinesTwoPipelineOptionsInstancesWithCorrectPrecedence()
+        {
+            // Arrange
+            string testPrimaryProject = "testPrimaryProject";
+            string testSecondaryProject = "testSecondaryProject";
+            string testSecondaryPipeline = "testSecondaryPipeline";
+
+            PipelineOptions primary = new PipelineOptions
+            {
+                Verbose = false,
+                Project = testPrimaryProject
+            };
+
+            PipelineOptions secondary = new PipelineOptions
+            {
+                Verbose = true,
+                DryRun = true,
+                Project = testSecondaryProject,
+                Pipeline = testSecondaryPipeline
+            };
+
+            // Act
+            primary.Combine(secondary);
+
+            // Assert
+            Assert.False(primary.Verbose);
+            Assert.True(primary.DryRun);
+            Assert.Equal(testPrimaryProject, primary.Project);
+            Assert.Equal(testSecondaryPipeline, primary.Pipeline);
+        }
+    }
+}
