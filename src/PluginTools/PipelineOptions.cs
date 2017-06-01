0f4f30a63e00a19b2ae8e437e952657dcd40a9af
JeremyTCD <jeremytanchongdao@gmail.com>
2017-06-01 20:35:53 +0800

    Made changes to PipelineOptions
    - Added Combine function
    - Added fields for verbose and dryrun so that it is possible to know when they are unset

--- src/PluginTools/PipelineOptions.cs
+++ src/PluginTools/PipelineOptions.cs
@@ -1,10 +1,62 @@
 ﻿namespace JeremyTCD.PipelinesCE.PluginTools
 {
     public class PipelineOptions
-    { 
-        public virtual bool Verbose { get; set; }
-        public virtual string Project { get; set; } = "PipelinesCE.csproj";
+    {
+        private bool? _verbose;
+        private bool? _dryRun;
+
+        public virtual bool Verbose
+        {
+            get
+            {
+                return _verbose ?? false;
+            }
+            set
+            {
+                _verbose = value;
+            }
+        }
+        public virtual bool DryRun
+        {
+            get
+            {
+                return _dryRun ?? false;
+            }
+            set
+            {
+                _dryRun = value;
+            }
+        }
+        public virtual string Project { get; set; }
         public virtual string Pipeline { get; set; }
-        public virtual bool DryRun { get; set; }
+
+        /// <summary>
+        /// Overwrites each property that has not been set with the value of its equivalent property in <paramref name="secondary"/>
+        /// </summary>
+        /// <param name="secondary"></param>
+        public virtual PipelineOptions Combine(PipelineOptions secondary)
+        {
+            if (_verbose == null)
+            {
+                Verbose = secondary.Verbose;
+            }
+
+            if (_dryRun == null)
+            {
+                DryRun = secondary.DryRun;
+            }
+
+            if (Project == null)
+            {
+                Project = secondary.Project;
+            }
+
+            if (Pipeline == null)
+            {
+                Pipeline = secondary.Pipeline;
+            }
+
+            return this;
+        }
     }
 }
