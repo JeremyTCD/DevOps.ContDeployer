831e9f459f565e90b3c2980435c6c7b6f109867b
JeremyTCD <jeremytanchongdao@gmail.com>
2017-06-03 21:49:35 +0800

    Added stub projects for testing PipelinesCE

---
+++ test/StubPipelinesCEProject/StubPipelineFactory.cs
@@ -0,0 +1,17 @@
+﻿using JeremyTCD.PipelinesCE;
+using JeremyTCD.PipelinesCE.PluginTools;
+using PipelinesCE.StubPluginProject;
+
+namespace PipelineCE.Tests.StubPipelinesCEProject
+{
+    public class StubPipelineFactory : IPipelineFactory
+    {
+        public Pipeline CreatePipeline()
+        {
+            return new Pipeline(new IStep[]
+            {
+                new Step<StubPlugin>(null)
+            });
+        }
+    }
+}
---
+++ test/StubPluginProject/StubPlugin.cs
@@ -0,0 +1,12 @@
+﻿using JeremyTCD.PipelinesCE.PluginTools;
+
+namespace PipelinesCE.StubPluginProject
+{
+    public class StubPlugin : IPlugin
+    {
+        public void Run(IPipelineContext pipelineContext, IStepContext stepContext)
+        {
+
+        }
+    }
+}
