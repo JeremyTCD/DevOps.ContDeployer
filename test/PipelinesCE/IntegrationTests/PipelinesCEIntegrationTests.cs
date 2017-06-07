73c46564aacc664f507d8811f55b849bdc7d01ac
JeremyTCD <jeremytanchongdao@gmail.com>
2017-06-07 22:33:35 +0800

    Made changes to PipelinesCEIntegrationTests
    - Now converts relative package reference paths to absolute paths after copying stub project
    - Now verfies PipelinesCE.Run output

--- test/PipelinesCE/IntegrationTests/PipelinesCEIntegrationTests.cs
+++ test/PipelinesCE/IntegrationTests/PipelinesCEIntegrationTests.cs
@@ -1,51 +1,91 @@
 ﻿using JeremyTCD.DotNetCore.Utils;
+using JeremyTCD.PipelinesCE.PluginTools;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using StructureMap;
+using System;
 using System.IO;
 using System.Reflection;
+using System.Xml;
 using Xunit;
 
 namespace JeremyTCD.PipelinesCE.Tests.IntegrationTests
 {
-    /// <summary>
-    /// The goal of these tests is to ensure that <see cref="ServiceCollectionExtensions"/> configures PipelinesCE
-    /// services properly.
-    /// </summary>
     public class PipelinesCEIntegrationTests
     {
         private DirectoryService _directoryService = new DirectoryService();
-        private string _tempDir { get; } = Path.Combine(Path.GetTempPath(), $"{nameof(PipelinesCE)}temp");
-        private IContainer _container { get; } 
-
-        private string _projectFileName { get; } = "PipelinesCE.csproj";
-        private string _assemblyName { get; } = "JeremyTCD.Test.dll";
+        private string _tempDir { get; } = Path.Combine(Path.GetTempPath(), $"{nameof(PipelinesCE)}Temp");
+        private IContainer _container { get; }
+        private ILoggerFactory _loggerFactory { get; }
 
         public PipelinesCEIntegrationTests()
         {
             _container = CreateContainer();
+            _loggerFactory = _container.GetInstance<ILoggerFactory>();
+            _loggerFactory.AddConsole();
             _directoryService.Delete(_tempDir, true);
             _directoryService.Create(_tempDir);
             _directoryService.SetCurrentDirectory(_tempDir);
         }
 
+        /// <summary>
+        /// This test covers the full breadth of <see cref="PipelinesCE"/>. In particular, it verifies that
+        /// <see cref="ServiceCollectionExtensions.AddPipelinesCE(IServiceCollection)"/> configures default PipelinesCE
+        /// services properly as well as that <see cref="PipelinesCE.Run(PipelineOptions)"/> configures per plugin
+        /// services properly.
+        /// </summary>
         [Fact]
-        public void Run_ConfiguresContainerAndCallsPipelineRunnerRunWithExpectedPipelineInstance()
+        public void Run_CallsPluginRun()
         {
-            // Copy both stub projects to tempdir
-            string currentAssemblyPath = Directory.GetParent(typeof(PipelinesCEIntegrationTests).GetTypeInfo().Assembly.Location).FullName;
-            string stubPipelinesCEProjectPath = Path.Combine(currentAssemblyPath, $"../../../../StubPipelinesCEProject");
-            string stubPluginProjectPath = Path.Combine(currentAssemblyPath, $"../../../../StubPluginProject");
+            // Arrange
+            // This magic string is unavoidable, can't reference this assembly since it is to be build and loaded by PipelinesCE
+            string solutionDir = Path.GetFullPath(typeof(PipelinesCEIntegrationTests).GetTypeInfo().Assembly.Location + "../../../../../../../");
+            string projectDir = "StubPipelinesCEProject";
+            string projectAbsSrcDir = Path.GetFullPath(solutionDir + $"test/{projectDir}");
+            string projectAbsDestDir = Path.Combine(_tempDir, projectDir);
+            // Copy stub project to temp dir
+            _directoryService.Copy(projectAbsSrcDir, projectAbsDestDir, excludePatterns: new string[] { "^bin$", "^obj$" });
+            // Replace relevant relative paths
+            string ProjectFile = Path.Combine(projectAbsDestDir, $"JeremyTCD.PipelinesCE.Tests.{projectDir}.csproj");
+            ConvertProjectReferenceRelPathsToAbs(ProjectFile, projectAbsSrcDir);
 
-            // TODO copy only shallow copies contents of specified directory, it should copy the entire directory
-            // allow for ignoring of specific files/folders such as bin and obj in this case
-            _directoryService.Copy(stubPipelinesCEProjectPath, _tempDir);
-            _directoryService.Copy(stubPluginProjectPath, _tempDir);
+            PipelinesCE pipelinesCE = _container.GetInstance<PipelinesCE>();
 
-            // Create child container, override PipelineRunner (for now)
-            // Run PipelinesCE in folder
+            StringWriter stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            // Act
+            pipelinesCE.Run(new PipelineOptions { Project = ProjectFile });
+
+            // Assert
+            _loggerFactory.Dispose();
+            stringWriter.Dispose();
+            string output = stringWriter.ToString();
+            // If these two logs were written to console, ServiceCollectionExtensions.AddPipelinesCE configures services properly and PipelinesCE.Run 
+            // configures per plugin services properly
+            Assert.Contains(string.Format(Strings.Log_PipelineComplete, "Stub"), output);
+            Assert.Contains(string.Format(Strings.Log_PluginComplete, "StubPlugin"), output);
         }
 
-        public IContainer CreateContainer()
+        private void ConvertProjectReferenceRelPathsToAbs(string projectFile, string projectDir)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            FileStream stream = new FileStream(projectFile, FileMode.Open);
+            doc.Load(stream);
+            stream.Dispose();
+
+            foreach (XmlNode node in doc.GetElementsByTagName("ProjectReference"))
+            {
+                node.Attributes["Include"].Value = Path.Combine(projectDir, node.Attributes["Include"].Value);
+            }
+
+            stream = new FileStream(projectFile, FileMode.Create);
+            doc.Save(stream);
+            stream.Dispose();
+        }
+
+        private IContainer CreateContainer()
         {
             ServiceCollection services = new ServiceCollection();
             services.AddPipelinesCE();
