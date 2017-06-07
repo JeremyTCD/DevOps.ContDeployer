bc9eaccdc1f35771cd9e2661e9574a1e6b7f421a
JeremyTCD <jeremytanchongdao@gmail.com>
2017-06-07 22:20:31 +0800

    Added CommandLineAppOptions

---
+++ src/CommandLineApp/CommandLineAppOptions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace JeremyTCD.PipelinesCE.CommandLineApp
+{
+    public class CommandLineAppOptions
+    {
+        public LogLevel DefaultMinLogLevel { get; } = LogLevel.Trace;
+        public LogLevel VerboseMinLogLevel { get; } = LogLevel.Information;
+        public string LogFileFormat { get; } = "log-{Date}.txt"; // https://github.com/serilog/serilog-extensions-logging-file
+    }
+}
