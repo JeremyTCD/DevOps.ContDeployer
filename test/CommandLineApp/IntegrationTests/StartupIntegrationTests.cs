e26474405b1a5866ccc110a2c349289e5a692451
JeremyTCD <jeremytanchongdao@gmail.com>
2017-06-19 21:36:26 +0800

    Added basic Integration test for Startup
    - Incomplete

---
+++ test/CommandLineApp/IntegrationTests/StartupIntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using StructureMap;
+using Xunit;
+
+namespace JeremyTCD.PipelinesCE.CommandLineApp.Tests.IntegrationTests
+{
+    /// <summary>
+    /// Tests to ensure that <see cref="Startup"/> handles service configuration correctly
+    /// </summary>
+    public class StartupIntegrationTests
+    {
+        [Fact]
+        public void ConfigureServices_ConfiguresServicesCorrectly()
+        {
+            // Arrange
+            Startup startup = new Startup();
+
+            // Act
+            IContainer claContainer = startup.ConfigureServices();
+
+            // Assert
+            IContainer pipelinesCEContainer = claContainer.GetInstance<IContainer>();
+        }
+
+        [Fact]
+        public void ConfigureServices_GeneratedContainerDisposesCorrectly()
+        {
+            // Arrange
+            Startup startup = new Startup();
+
+            // Act
+            IContainer claContainer = startup.ConfigureServices();
+
+            // Assert
+            IContainer pipelinesCEContainer = claContainer.GetInstance<IContainer>();
+        }
+    }
+}
