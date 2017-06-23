3bd1c65adb14cf4cac4c40551d7555732d3c2c08
JeremyTCD <jeremytanchongdao@gmail.com>
2017-06-23 18:02:48 +0800

    Fleshed out CommandLineAppRegistryIntegrationTests

--- test/CommandLineApp/IntegrationTests/CommandLineAppRegistryIntegrationTests.cs
+++ test/CommandLineApp/IntegrationTests/CommandLineAppRegistryIntegrationTests.cs
@@ -1,35 +1,91 @@
-﻿using StructureMap;
+﻿using JeremyTCD.DotNetCore.Utils;
+using JeremyTCD.PipelinesCE.PluginTools;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StructureMap;
+using StructureMap.Pipeline;
+using StructureMap.Query;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace JeremyTCD.PipelinesCE.CommandLineApp.Tests.IntegrationTests
 {
     /// <summary>
-    /// Tests to ensure that <see cref="Startup"/> handles service configuration correctly
+    /// These tests ensure that CommandLineAppRegistry behaves correctly
     /// </summary>
     public class CommandLineAppRegistryIntegrationTests
     {
         [Fact]
-        public void ConfigureServices_ConfiguresServicesCorrectly()
+        public void CommandLineAppRegistry_ConfiguresServicesCorrectly()
         {
-            // Arrange
-
-            // Act
-            IContainer container = new Container(new CommandLineAppRegistry());
+            // Arrange and Act
+            Container container = new Container(new CommandLineAppRegistry());
 
             // Assert
-            IContainer pipelinesCEContainer = container.GetInstance<IContainer>();
+            IModel model = container.Model;
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(ILoggerFactory) && r.ReturnedType == typeof(LoggerFactory)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(ILogger<>) && r.ReturnedType == typeof(Logger<>)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IOptions<>) && r.ReturnedType == typeof(OptionsManager<>)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IOptionsMonitor<>) && r.ReturnedType == typeof(OptionsMonitor<>)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IOptionsSnapshot<>) && r.ReturnedType == typeof(OptionsSnapshot<>)
+                && r.Lifecycle.GetType() == typeof(ContainerLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IOptionsFactory<>) && r.ReturnedType == typeof(OptionsFactory<>)
+                && r.Lifecycle.GetType() == typeof(UniquePerRequestLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IOptionsCache<>) && r.ReturnedType == typeof(OptionsCache<>)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IAssemblyService) && r.ReturnedType == typeof(AssemblyService)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IPathService) && r.ReturnedType == typeof(PathService)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IDirectoryService) && r.ReturnedType == typeof(DirectoryService)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IMSBuildService) && r.ReturnedType == typeof(MSBuildService)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IActivatorService) && r.ReturnedType == typeof(ActivatorService)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(ILoggingService<>) && r.ReturnedType == typeof(LoggingService<>)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IProcessService) && r.ReturnedType == typeof(ProcessService)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(ICommandLineUtilsService) && r.ReturnedType == typeof(CommandLineUtilsService)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IPipelineRunner) && r.ReturnedType == typeof(PipelineRunner)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IPipelineContext) && r.ReturnedType == typeof(PipelineContext)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IStepContextFactory) && r.ReturnedType == typeof(StepContextFactory)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(IPipelineContextFactory) && r.ReturnedType == typeof(PipelineContextFactory)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(PipelinesCE) && r.ReturnedType == typeof(PipelinesCE)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(RunCommand) && r.ReturnedType == typeof(RunCommand)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
+            Assert.True(model.AllInstances.Any(r => r.PluginType == typeof(RootCommand) && r.ReturnedType == typeof(RootCommand)
+                && r.Lifecycle.GetType() == typeof(SingletonLifecycle)));
         }
 
         [Fact]
-        public void ConfigureServices_GeneratedContainerDisposesCorrectly()
+        public void CommandLineAppRegistry_GeneratedContainerDisposesCorrectly()
         {
-            // Arrange
-
-            // Act
-            IContainer claContainer = new Container(new CommandLineAppRegistry());
+            // Arrange and Act
+            Container container = new Container(new CommandLineAppRegistry());
+            PipelinesCE pipelinesCE = container.GetInstance<PipelinesCE>();
+            ILoggerFactory loggerFactory = container.GetInstance<ILoggerFactory>();
+            container.Dispose();
 
             // Assert
-            IContainer pipelinesCEContainer = claContainer.GetInstance<IContainer>();
+            Assert.True(pipelinesCE.IsDisposed);
+            Assert.Throws<ObjectDisposedException>(() => loggerFactory.AddProvider(null));
         }
     }
 }
