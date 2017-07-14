f6be161f687a4e0b9340467d7cef8cdd4c1a6350
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-14 22:05:07 +0800

    Added some comments to PipelineOptions

--- src/PluginAndConfigTools/PipelineOptions.cs
+++ src/PluginAndConfigTools/PipelineOptions.cs
@@ -1,5 +1,12 @@
-﻿namespace JeremyTCD.PipelinesCE.PluginAndConfigTools
+﻿using Newtonsoft.Json;
+
+namespace JeremyTCD.PipelinesCE.PluginAndConfigTools
 {
+    /// <summary>
+    /// For backward and forward compatibility, all properties must have a default value.
+    /// Properties must have nullable backing fields so there is some state that represents unset. 
+    /// This facilitates overwriting of unset properties, <see cref="Combine(PipelineOptions)"/>.
+    /// </summary>
     public class PipelineOptions
     {
         public const string DefaultPipeline = "Default";
