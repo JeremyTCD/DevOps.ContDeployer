f63d33152e202cb7821bf58ca6a90dd79fb32e07
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-14 22:05:30 +0800

    Started work on PrivateFieldsJsonConverter

---
+++ src/PluginAndConfigTools/PrivateFieldsJsonConverter.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace JeremyTCD.PipelinesCE.PluginAndConfigTools
+{
+    // TODO move this to utils
+    /// <summary>
+    /// Serializes private fields in root object
+    /// 
+    /// Inspired by https://stackoverflow.com/questions/30300740/how-to-configure-json-net-deserializer-to-track-missing-properties
+    /// </summary>
+    public class PrivateFieldsJsonConverter : JsonConverter
+    {
+        public List<FieldInfo> MissingFields { get; } = new List<FieldInfo>();
+        public List<KeyValuePair<string, JToken>> ExtraFields { get; } = new List<KeyValuePair<string, JToken>>();
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            existingValue = existingValue ?? Activator.CreateInstance(objectType, true);
+
+            JObject jObject = JObject.Load(reader);
+            FieldInfo[] privateFields = objectType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (FieldInfo field in privateFields)
+            {
+                JToken jToken = jObject[field.Name];
+
+                // Missing field
+                if (jToken == null)
+                {
+                    MissingFields.Add(field);
+                    continue;
+                }
+
+                jObject.Remove(field.Name);
+                object value = jToken.ToObject(field.FieldType);
+
+                field.SetValue(existingValue, value);
+            }
+
+            foreach (KeyValuePair<string, JToken> pair in jObject)
+            {
+                // Extra field
+                ExtraFields.Add(pair);
+            }
+
+            return existingValue;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            Type objectType = value.GetType();
+            FieldInfo[] privateFields = objectType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            JsonSerializer secondarySerializer = new JsonSerializer();
+
+            writer.WriteStartObject();
+
+            foreach (FieldInfo field in privateFields)
+            {
+                writer.WritePropertyName(field.Name);
+                object fieldValue = field.GetValue(value);
+                // TODO what if value is serializable?
+                // https://stackoverflow.com/questions/26129448/json-net-how-to-customize-serialization-to-insert-a-json-property
+                // https://github.com/JamesNK/Newtonsoft.Json/issues/386
+                secondarySerializer.Serialize(writer, fieldValue);
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+}
---
+++ test/PluginAndConfigTools/IntegrationTests/PrivateFieldsJsonConverterIntegrationTests.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System;
+using Xunit;
+
+namespace JeremyTCD.PipelinesCE.PluginAndConfigTools.Tests.IntegrationTests
+{
+    public class PrivateFieldsJsonConverterIntegrationTests
+    {
+        [Fact]
+        public void Serialize_SerializesPrivateFields()
+        {
+            // Arrange
+            int testField1Value = 1;
+            string testField2Value = "testField2Value";
+            bool? testField3Value = true;
+
+            StubClass stubClass = new StubClass
+            {
+                StubProperty1 = testField1Value,
+                StubProperty2 = testField2Value,
+                StubProperty3 = testField3Value
+            };
+
+            // Act
+            string result = JsonConvert.SerializeObject(stubClass, new PrivateFieldsJsonConverter());
+
+            // Assert
+            //Assert.Contains("\"_stubField1", result);
+        }
+
+        private class StubClass
+        {
+            private int _stubField1;
+            private string _stubField2;
+            private bool? _stubField3;
+            public int _stubField4;
+
+            public int StubProperty1
+            {
+                get
+                {
+                    return _stubField1;
+                }
+                set
+                {
+                    _stubField1 = value;
+                }
+            }
+
+            public string StubProperty2
+            {
+                get
+                {
+                    return _stubField2;
+                }
+                set
+                {
+                    _stubField2 = value;
+                }
+            }
+
+            public bool? StubProperty3
+            {
+                get
+                {
+                    return _stubField3;
+                }
+                set
+                {
+                    _stubField3 = value;
+                }
+            }
+        }
+
+        //[Fact]
+        //public void ParseArgs_DeserializesAndLogsIfArgsAreFromAPipelineOptionsVersionWithFewerMembers()
+        //{
+        //    // Arrange
+        //    string testPipeline = "testPipeline";
+        //    string testProject = "testProject";
+        //    bool dryRun = true;
+
+        //    PipelineOptions stubPipelineOptions = new PipelineOptions
+        //    {
+        //        Pipeline = testPipeline,
+        //        Project = testProject,
+        //        DryRun = dryRun
+        //    };
+
+        //    string json = JsonConvert.SerializeObject(stubPipelineOptions, new JsonSerializerSettings { ContractResolver = new PipelineOptionsContractResolver() });
+        //    JObject obj = JObject.Parse(json);
+        //    obj.Property("_pipeline").Remove();
+        //    json = obj.ToString();
+
+        //    // Act
+        //    PipelineOptions result = Program.ParseArgs(new string[] { json });
+
+        //    // Assert
+        //    Assert.Equal(PipelineOptions.DefaultPipeline, result.Pipeline);
+        //    Assert.Equal(testProject, result.Project);
+        //    Assert.Equal(dryRun, result.DryRun);
+        //}
+
+        //// TODO same members but different member types?
+        //[Fact]
+        //public void ParseArgs_Deserializes()
+        //{
+        //    // Arrange
+        //    string testPipeline = "testPipeline";
+        //    PipelineOptions stubPipelineOptions = new PipelineOptions
+        //    {
+        //        Pipeline = testPipeline
+        //    };
+
+        //    string json = JsonConvert.SerializeObject(stubPipelineOptions, new JsonSerializerSettings { ContractResolver = new PipelineOptionsContractResolver() });
+
+        //    // Act
+        //    PipelineOptions result = Program.ParseArgs(new string[] { json });
+
+        //    // Assert
+        //    Assert.Equal(PipelineOptions.DefaultDryRun, result.DryRun);
+        //    Assert.Equal(testPipeline, result.Pipeline);
+        //    Assert.Equal(PipelineOptions.DefaultProject, result.Project);
+        //}
+    }
+}
