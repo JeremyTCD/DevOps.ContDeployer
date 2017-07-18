183699bf59bfc871644f535de690d4ea0a66c529
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-18 17:54:42 +0800

    Added tests for PipelineRunner.Program.Configure

--- test/PipelineRunner/IntegrationTests/ProgramIntegrationTests.cs
+++ test/PipelineRunner/IntegrationTests/ProgramIntegrationTests.cs
@@ -1,9 +1,12 @@
 ﻿using JeremyTCD.DotNetCore.Utils;
 using JeremyTCD.Newtonsoft.Json.Utils;
 using JeremyTCD.PipelinesCE.PluginAndConfigTools;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using StructureMap;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace JeremyTCD.PipelinesCE.PipelineRunner.Tests.IntegrationTests
@@ -72,16 +75,40 @@ namespace JeremyTCD.PipelinesCE.PipelineRunner.Tests.IntegrationTests
             Assert.Equal(testPipeline, options.Pipeline);
             Assert.Equal(PipelineOptions.DefaultVerbose, options.Verbose);
             Assert.Equal(PipelineOptions.DefaultProject, options.Project);
-            Assert.Equal(string.Format(Strings.Log_ExecutableAndProjectVersionsDoNotMatch, 
-                Environment.NewLine + Program.NormalizeFieldName(extraFieldKey), 
-                Environment.NewLine + Program.NormalizeFieldName(projectFieldKey)), 
+            Assert.Equal(string.Format(Strings.Log_ExecutableAndProjectVersionsDoNotMatch,
+                Environment.NewLine + Program.NormalizeFieldName(extraFieldKey),
+                Environment.NewLine + Program.NormalizeFieldName(projectFieldKey)),
                 warnings);
         }
 
-        [Fact]
-        public void Configure_ConfiguresLoggerFactoryCorrectly()
+        [Theory]
+        [MemberData(nameof(ConfiguresLoggerFactoryData))]
+        public void Configure_ConfiguresLoggerFactoryCorrectly(LogLevel minLogLevel, bool verbose)
         {
-            // TODO cant mock extensions methods using moq
+            // Arrange
+            IContainer container = new Container(r =>
+            {
+                r.For<ILoggerFactory>().Singleton().Use<LoggerFactory>();
+            });
+
+            PipelineOptions pipelineOptions = new PipelineOptions
+            {
+                Verbose = verbose
+            };
+
+            // Act
+            Program.Configure(container, pipelineOptions);
+
+            // Assert
+            ILoggerFactory loggerFactory = container.GetInstance<ILoggerFactory>();
+            ILogger logger = loggerFactory.CreateLogger("");
+            Assert.True(logger.IsEnabled(minLogLevel) && (minLogLevel == LogLevel.Trace || !logger.IsEnabled(minLogLevel - 1)));
+        }
+
+        public static IEnumerable<object[]> ConfiguresLoggerFactoryData()
+        {
+            yield return new object[] { PipelineOptions.VerboseMinLogLevel, true };
+            yield return new object[] { PipelineOptions.DefaultMinLogLevel, false };
         }
     }
 }
