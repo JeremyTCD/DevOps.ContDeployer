d6253d6dda0f2c148819e6eaf1e934c38198f4f7
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-27 14:48:02 +0800

    Added tests for CommandLineAppServiceCollectionExtensions and ConfigHostServiceCollectionExtensions

---
+++ test/CommandLineApp/IntegrationTests/CommandLineAppServiceCollectionExtensionsIntegrationTests.cs
@@ -0,0 +1,28 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Xunit;
+
+namespace JeremyTCD.PipelinesCE.CommandLineApp.Tests.IntegrationTests
+{
+    /// <summary>
+    /// These tests ensure that CommandLineAppRegistry behaves correctly
+    /// </summary>
+    public class CommandLineAppServiceCollectionExtensionsIntegrationTests
+    {
+        [Fact]
+        public void AddCommandLineApp_ConfiguresServicesCorrectly()
+        {
+            // Arrange
+            ServiceCollection services = new ServiceCollection();
+
+            // Act
+            services.AddCommandLineApp();
+
+            // Assert
+            ServiceDescriptorComparer comparer = new ServiceDescriptorComparer();
+            Assert.True(services.Contains(ServiceDescriptor.Singleton<RunCommand, RunCommand>(), comparer));
+            Assert.True(services.Contains(ServiceDescriptor.Singleton<RootCommand, RootCommand>(), comparer));
+        }
+    }
+}
---
+++ test/ConfigHost/IntegrationTests/ConfigHostServiceCollectionExtensionsIntegrationTests.cs
@@ -0,0 +1,44 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using StructureMap;
+using System.Linq;
+using Xunit;
+
+namespace JeremyTCD.PipelinesCE.ConfigHost.Tests.IntegrationTests
+{
+    public class ConfigHostServiceCollectionExtensionsIntegrationTests
+    {
+        [Fact]
+        public void AddConfigHost_ConfiguresServicesCorrectly()
+        {
+            // Arrange 
+            ServiceCollection services = new ServiceCollection();
+
+            // Act
+            services.AddConfigHost();
+
+            // Assert
+            ServiceDescriptorComparer comparer = new ServiceDescriptorComparer();
+            Assert.True(services.Contains(ServiceDescriptor.Singleton<ConfigHostCore, ConfigHostCore>(), comparer));
+            Assert.True(services.Contains(ServiceDescriptor.Singleton<IPipelineLoader, PipelineLoader>(), comparer));
+            Assert.True(services.Contains(ServiceDescriptor.Singleton<IPipelineRunner, PipelineRunner>(), comparer));
+        }
+
+        [Fact]
+        public void AddConfigHost_GeneratedContainerDisposesCorrectly()
+        {
+            // Arrange
+            ServiceCollection services = new ServiceCollection();
+            services.AddConfigHost();
+            Container container = new Container(r => r.Populate(services)); // PipelineLoader requires an IContainer instance
+
+            PipelineLoader pipelineLoader = container.GetInstance<IPipelineLoader>() as PipelineLoader;
+
+            // Act
+            container.Dispose();
+
+            // Assert
+            Assert.True(pipelineLoader.IsDisposed);
+        }
+    }
+}
