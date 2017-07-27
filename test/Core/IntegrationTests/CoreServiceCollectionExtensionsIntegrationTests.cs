a20afa14f0e37a5b3c6394ab9b2e828fe56c0f2b
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-27 14:52:43 +0800

    Added tests for CoreServiceCollectionExtensions

---
+++ test/Core/IntegrationTests/CoreServiceCollectionExtensionsIntegrationTests.cs
@@ -0,0 +1,25 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Xunit;
+
+namespace JeremyTCD.PipelinesCE.Core.Tests.IntegrationTests
+{
+    public class CoreServiceCollectionExtensionsIntegrationTests
+    {
+        [Fact]
+        public void AddCore_ConfiguresServicesCorrectly()
+        {
+            // Arrange 
+            ServiceCollection services = new ServiceCollection();
+
+            // Act
+            services.AddCore();
+
+            // Assert
+            ServiceDescriptorComparer comparer = new ServiceDescriptorComparer();
+            Assert.True(services.Contains(ServiceDescriptor.Singleton<IStepContextFactory, StepContextFactory>(), comparer));
+            Assert.True(services.Contains(ServiceDescriptor.Singleton<IPipelineContextFactory, PipelineContextFactory>(), comparer));
+        }
+    }
+}
