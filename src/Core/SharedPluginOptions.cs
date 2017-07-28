21378f34f8a0928092be6bd7ebde86ee24de58f7
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-28 17:31:36 +0800

    Added comments to SharedPluginOptions

--- src/Core/SharedPluginOptions.cs
+++ src/Core/SharedPluginOptions.cs
@@ -1,5 +1,10 @@
 ﻿namespace JeremyTCD.PipelinesCE.Core
 {
+    /// <summary>
+    /// For backward and forward compatibility, all properties must have a default value.
+    /// Properties must have nullable backing fields so there is some state that represents unset. 
+    /// This facilitates overwriting of unset properties, <see cref="Combine(PipelinesCEOptions)"/>.
+    /// </summary>
     public class SharedPluginOptions
     {
         public const bool DefaultDryRun = false;
