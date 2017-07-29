08790969ae53e93b9f863cc4a507904cf9ef716f
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-29 21:50:55 +0800

    Added StubPlugin.Logger project

---
+++ test/StubPlugin.Logger/StubLogger.cs
@@ -0,0 +1,38 @@
+﻿using JeremyTCD.DotNetCore.Utils;
+using JeremyTCD.PipelinesCE.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace StubPlugin.Logger
+{
+    public class StubLogger
+    {
+        private ILogger _logger { get; set; }
+
+        public void Configure(string logFile, string loggerName)
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddUtils();
+
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            IPathService pathService = serviceProvider.GetService<IPathService>();
+            ILoggerFactory loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+
+            PipelinesCEOptions pipelinesCEOptions = new PipelinesCEOptions
+            {
+                FileLogging = true,
+                LogFile = logFile
+            };
+
+            LoggingConfig loggingConfig = new LoggingConfig(pathService, null);
+            loggingConfig.Configure(loggerFactory, pipelinesCEOptions);
+
+            _logger = loggerFactory.CreateLogger(loggerName);
+        }
+
+        public void LogInformation(string message)
+        {
+            _logger.LogInformation(message);
+        }
+    }
+}
