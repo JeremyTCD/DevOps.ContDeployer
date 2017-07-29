2be69c5d65a1e2dd6cd63d0bed9e8934911949d2
JeremyTCD <jeremytanchongdao@gmail.com>
2017-07-29 16:00:56 +0800

    Completed unit tests for LoggingConfig

--- test/Core/UnitTests/LoggingConfigUnitTests.cs
+++ test/Core/UnitTests/LoggingConfigUnitTests.cs
@@ -19,23 +19,35 @@ namespace JeremyTCD.PipelinesCE.Core.Tests.UnitTests
             _mockRepository = new MockRepository(MockBehavior.Default) { DefaultValue = DefaultValue.Mock };
         }
 
+        [Fact]
+        public void CreateLoggingConfiguration_CreatesLoggingConfig()
+        {
+            //Arrange
+            PipelinesCEOptions stubPipelinesCEOptions = new PipelinesCEOptions();
+
+            LoggingConfig loggingConfig = new LoggingConfig(null, null);
+
+            // Act
+            LoggingConfiguration result = loggingConfig.CreateLoggingConfiguration(stubPipelinesCEOptions);
+
+            // Assert
+            _mockRepository.VerifyAll();
+            Assert.NotNull(result);
+            Assert.Equal(1, result.ConfiguredNamedTargets.Count);
+        }
+
         [Theory]
         [MemberData(nameof(SetsLogLevelAccordingToDebugAndVerboseOptionsData))]
         public void CreateLoggingConfiguration_SetsLogLevelAccordingToDebugAndVerboseOptions(bool debug, bool verbose, LogLevel expectedLogLevel)
         {
             // Arrange
-            string testLogFile = "testLogFile";
             PipelinesCEOptions stubPipelinesCEOptions = new PipelinesCEOptions
             {
                 Debug = debug,
-                Verbose = verbose,
-                LogFile = testLogFile
+                Verbose = verbose
             };
 
-            Mock<IPathService> mockPathService = _mockRepository.Create<IPathService>();
-            mockPathService.Setup(p => p.IsPathRooted(testLogFile)).Returns(true);
-
-            LoggingConfig loggingConfig = new LoggingConfig(mockPathService.Object, null);
+            LoggingConfig loggingConfig = new LoggingConfig(null, null);
 
             // Act
             LoggingConfiguration result = loggingConfig.CreateLoggingConfiguration(stubPipelinesCEOptions);
@@ -54,53 +66,13 @@ namespace JeremyTCD.PipelinesCE.Core.Tests.UnitTests
         }
 
         [Fact]
-        public void CreateLoggingConfiguration_RootsLogFileIfItIsNotRooted()
-        {
-            // Arrange
-            string testLogFile = "testLogFile";
-            string testProjectFile = "testProjectFile";
-            PipelinesCEOptions stubPipelinesCEOptions = new PipelinesCEOptions
-            {
-                LogFile = testLogFile,
-                ProjectFile = testProjectFile
-            };
-            DirectoryInfo stubDirectoryInfo = new DirectoryInfo("C:/test");
-            string testProjectDir = stubDirectoryInfo.FullName;
-
-            Mock<IPathService> mockPathService = _mockRepository.Create<IPathService>();
-            mockPathService.Setup(p => p.IsPathRooted(testLogFile)).Returns(false);
-            mockPathService.Setup(p => p.Combine(testProjectDir, testLogFile)).Returns(testLogFile);
-
-            Mock<IDirectoryService> mockDirectoryService = _mockRepository.Create<IDirectoryService>();
-            mockDirectoryService.Setup(d => d.GetParent(testProjectFile)).Returns(stubDirectoryInfo);
-
-            LoggingConfig loggingConfig = new LoggingConfig(mockPathService.Object, mockDirectoryService.Object);
-
-            // Act
-            LoggingConfiguration result = loggingConfig.CreateLoggingConfiguration(stubPipelinesCEOptions);
-
-            // Assert
-            _mockRepository.VerifyAll();
-        }
-
-        [Fact]
-        public void CreateLoggingConfiguration_CreatesLoggingConfig()
-        {
-            // TODO creates console target
-        }
-
-        // TODO update function so filetarget isn't created unless logfile is specified, write test 
-
-        [Theory]
-        [MemberData(nameof(CreatesDebuggerTargetIfDebugOrVerboseOptionsAreTrueData))]
-        public void CreateLoggingConfiguration_CreatesDebuggerTargetIfDebugOrVerboseOptionsAreTrue(bool debug, bool verbose, bool expected)
+        public void CreateLoggingConfiguration_CreatesFileTargetIfFileLoggingOptionsIsTrue()
         {
             // Arrange
             string testLogFile = "testLogFile";
             PipelinesCEOptions stubPipelinesCEOptions = new PipelinesCEOptions
             {
-                Debug = debug,
-                Verbose = verbose,
+                FileLogging = true,
                 LogFile = testLogFile
             };
 
@@ -114,15 +86,72 @@ namespace JeremyTCD.PipelinesCE.Core.Tests.UnitTests
 
             // Assert
             _mockRepository.VerifyAll();
-            Assert.Equal(expected, result.ConfiguredNamedTargets.Any(r => r.GetType() == typeof(DebuggerTarget)));
+            Assert.True(result.ConfiguredNamedTargets.Any(t => t.GetType() == typeof(FileTarget)));
+        }
+
+        [Fact]
+        public void CreateLoggingConfiguration_RootsLogFileIfItIsNotRooted()
+        {
+            //Arrange
+            string testLogFile = "testLogFile";
+            string testLogFileRooted = $"C:/{testLogFile}";
+            string testProjectFile = "C:/testProjectFile";
+            PipelinesCEOptions stubPipelinesCEOptions = new PipelinesCEOptions
+            {
+                FileLogging = true,
+                LogFile = testLogFile,
+                ProjectFile = testProjectFile
+            };
+            DirectoryInfo stubDirectoryInfo = new DirectoryInfo("C:/");
+            string testProjectDir = stubDirectoryInfo.FullName;
+
+            Mock<IPathService> mockPathService = _mockRepository.Create<IPathService>();
+            mockPathService.Setup(p => p.IsPathRooted(testLogFile)).Returns(false);
+            mockPathService.Setup(p => p.Combine(testProjectDir, testLogFile)).Returns(testLogFileRooted);
+
+            Mock<IDirectoryService> mockDirectoryService = _mockRepository.Create<IDirectoryService>();
+            mockDirectoryService.Setup(d => d.GetParent(testProjectFile)).Returns(stubDirectoryInfo);
+
+            LoggingConfig loggingConfig = new LoggingConfig(mockPathService.Object, mockDirectoryService.Object);
+
+            // Act
+            LoggingConfiguration result = loggingConfig.CreateLoggingConfiguration(stubPipelinesCEOptions);
+
+            // Assert
+            _mockRepository.VerifyAll();
+            FileTarget fileTarget = result.ConfiguredNamedTargets.Single(t => t.GetType() == typeof(FileTarget)) as FileTarget;
+            Assert.NotNull(fileTarget);
+            Assert.Equal($"'{testLogFileRooted}'", fileTarget.FileName.ToString());
+        }
+
+        [Theory]
+        [MemberData(nameof(CreatesDebuggerTargetIfDebugOrVerboseOptionsAreTrueData))]
+        public void CreateLoggingConfiguration_CreatesDebuggerTargetIfDebugOrVerboseOptionsAreTrue(bool debug, bool verbose)
+        {
+            // Arrange
+            string testLogFile = "testLogFile";
+            PipelinesCEOptions stubPipelinesCEOptions = new PipelinesCEOptions
+            {
+                Debug = debug,
+                Verbose = verbose,
+                LogFile = testLogFile
+            };
+
+            LoggingConfig loggingConfig = new LoggingConfig(null, null);
+
+            // Act
+            LoggingConfiguration result = loggingConfig.CreateLoggingConfiguration(stubPipelinesCEOptions);
+
+            // Assert
+            _mockRepository.VerifyAll();
+            Assert.True(result.ConfiguredNamedTargets.Any(r => r.GetType() == typeof(DebuggerTarget)));
         }
 
         public static IEnumerable<object[]> CreatesDebuggerTargetIfDebugOrVerboseOptionsAreTrueData()
         {
-            yield return new object[] { true, true, true };
-            yield return new object[] { false, true, true };
-            yield return new object[] { true, false, true};
-            yield return new object[] { false, false, false };
+            yield return new object[] { true, true};
+            yield return new object[] { false, true};
+            yield return new object[] { true, false};
         }
     }
 }
