36bfe1408fcbfab6607a3b880356a7e0f8080d0f
JeremyTCD <jeremytanchongdao@gmail.com>
2017-08-26 22:17:42 +0800

    Added ActionStep

---
+++ src/Core/ActionStep.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace JeremyTCD.PipelinesCE.Core
+{
+    public class ActionStep : Step
+    {
+        private Action<IPipelineContext, CancellationToken> _action;
+
+        public ActionStep(string name, IEnumerable<Step> dependencies, Action<IPipelineContext, CancellationToken> action) : base(name, dependencies)
+        {
+            _action = action;
+        }
+
+        public override void Run(IPipelineContext pipelineContext, CancellationToken cancellationToken)
+        {
+            _action(pipelineContext, cancellationToken);
+        }
+    }
+}
