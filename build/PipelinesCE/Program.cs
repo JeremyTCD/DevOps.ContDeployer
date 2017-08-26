7295a8e0f6e32412d94bb90cc3a5b31fafb0e230
JeremyTCD <jeremytanchongdao@gmail.com>
2017-08-26 22:19:27 +0800

    Added Program.cs to PipelinesCEConfig and updated PipelinesCEConfig.csproj
    - Now a console application

---
+++ build/PipelinesCE/Program.cs
@@ -0,0 +1,14 @@
+﻿using JeremyTCD.PipelinesCE.Config;
+
+namespace PipelinesCEConfig
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            ConfigProgram host = new ConfigProgram(); // TODO provide a builder instead 
+
+            return host.Run(args);
+        }
+    }
+}
