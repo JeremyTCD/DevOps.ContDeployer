8e9e29fd7fc45010b9b279552a8beca662bc1f52
JeremyTCD <jeremytanchongdao@gmail.com>
2017-08-26 22:17:27 +0800

    Added some properties to Step

--- src/Core/Step.cs
+++ src/Core/Step.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace JeremyTCD.PipelinesCE.Core
@@ -10,16 +12,24 @@ namespace JeremyTCD.PipelinesCE.Core
         public string Name { get; }
         public List<Step> Dependencies { get; }
         public List<Step> Dependents { get; }
+
         public Task Task { get; set; }
         public ILogger Logger { get; set; }
 
-        public Step(string name, IEnumerable<Step> dependencies = null)
+        public bool RunEvenIfFaultOccurs { get; }
+
+        public DateTime StartTime { get; }
+        public DateTime EndTime { get; }
+        public DateTime Duration { get; }
+
+        public Step(string name, IEnumerable<Step> dependencies = null, bool runEvenIfFaultOccurs = false)
         {
             Name = name;
             Dependencies = dependencies != null ? dependencies.ToList() : new List<Step>();
             Dependents = new List<Step>();
+            RunEvenIfFaultOccurs = runEvenIfFaultOccurs;
         }
 
-        public abstract void Run(IPipelineContext pipelineContext);
+        public abstract void Run(IPipelineContext pipelineContext, CancellationToken cancellationToken);
     }
 }
