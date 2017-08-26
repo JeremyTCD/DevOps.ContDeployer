5bdaf05abe6cde878160e6549c12fa218e59f348
JeremyTCD <jeremytanchongdao@gmail.com>
2017-08-26 22:18:01 +0800

    Cleaned up PipelineRunner and added tests

--- src/Core/PipelineRunner.cs
+++ src/Core/PipelineRunner.cs
@@ -8,20 +8,23 @@ namespace JeremyTCD.PipelinesCE.Core
     public class PipelineRunner : IPipelineRunner
     {
         private IStepGraphFactory _stepGraphFactory { get; }
+        private CancellationTokenSource _cts { get; set; }
         public ILoggerFactory _loggerFactory { get; }
 
         public PipelineRunner(IStepGraphFactory stepGraphFactory, ILoggerFactory loggerFactory)
         {
             _stepGraphFactory = stepGraphFactory;
             _loggerFactory = loggerFactory;
+            _cts = new CancellationTokenSource();
         }
 
-        // TODO rewrite
         /// <summary>
-        /// Runs steps. If an exception is thrown, all steps that have not started are labelled as cancelled. Also, a shared <see cref="CancellationToken"/>
-        /// has its <see cref="CancellationToken.IsCancellationRequested"/> set to true, allowing individual steps to 
-        /// cancel as soon as possible.
+        /// Runs steps. 
+        /// 
+        /// If an exception is thrown, all steps that have not started and that do not have <see cref="Step.RunEvenIfFaultOccurs"/> set to true
+        /// are cancelled. Also, running steps can poll <see cref="CancellationToken.IsCancellationRequested"/> to exit gracefully.
         /// </summary>
+        /// <param name="pipeline"></param>
         /// <param name="pipelineContext"></param>
         public void Run(Pipeline pipeline, IPipelineContext pipelineContext)
         {
@@ -37,31 +40,38 @@ namespace JeremyTCD.PipelinesCE.Core
             // Sort steps topologically
             stepGraph.SortTopologically();
 
-            // Create CancellationTokenSource
-            using (CancellationTokenSource cts = new CancellationTokenSource())
+            // Run steps
+            try
             {
-                // Run steps
                 foreach (Step step in stepGraph)
                 {
                     if (step.Dependencies.Count == 0)
                     {
-                        step.Task = Task.Factory.StartNew(() => RunStep(step, pipelineContext, cts),
-                            step.RunEvenIfFaultOccurs ? CancellationToken.None : cts.Token);
+                        step.Task = Task.Factory.StartNew(() => RunStep(step, pipelineContext, _cts),
+                            step.RunEvenIfFaultOccurs ? CancellationToken.None : _cts.Token);
                     }
                     else
                     {
-
                         step.Task = Task.Factory.ContinueWhenAll(step.Dependencies.Select(p => p.Task).ToArray(),
-                            (Task[] antecedents) => RunStep(step, pipelineContext, cts, antecedents),
-                            step.RunEvenIfFaultOccurs ? CancellationToken.None : cts.Token);
+                            _ => RunStep(step, pipelineContext, _cts),
+                            step.RunEvenIfFaultOccurs ? CancellationToken.None : _cts.Token);
                     }
                 }
 
                 Task.WaitAll(stepGraph.Select(s => s.Task).ToArray());
             }
+            finally
+            {
+                _cts.Dispose();
+            }
         }
 
-        private void RunStep(Step step, IPipelineContext pipelineContext, CancellationTokenSource cts, Task[] antecedents = null)
+        public void Cancel()
+        {
+            _cts.Cancel();
+        }
+
+        private void RunStep(Step step, IPipelineContext pipelineContext, CancellationTokenSource cts)
         {
             try
             {
--- test/Core/IntegrationTests/PipelineRunnerIntegrationTests.cs
+++ test/Core/IntegrationTests/PipelineRunnerIntegrationTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
@@ -10,18 +11,62 @@ namespace JeremyTCD.PipelinesCE.Core.Tests.IntegrationTests
 {
     public class PipelineRunnerIntegrationTests
     {
-        private PipelineRunner _pipelineRunner { get; }
         private MockRepository _mockRepository { get; }
+        private Mock<ILoggerFactory> _mockLoggerFactory { get; }
 
         public PipelineRunnerIntegrationTests()
         {
             _mockRepository = new MockRepository(MockBehavior.Default) { DefaultValue = DefaultValue.Mock };
-            Mock<ILoggerFactory> mockLoggerFactory = _mockRepository.Create<ILoggerFactory>();
-            _pipelineRunner = new PipelineRunner(new StepGraphFactory(), mockLoggerFactory.Object);
+            _mockLoggerFactory = _mockRepository.Create<ILoggerFactory>();
         }
 
         [Fact]
-        public void Run_IfATaskThrowsAnExceptionTasksThatHaveNotStartedAreCancelled()
+        public void Run_RunsAllStepsInCorrectOrder()
+        {
+            // Arrange
+            NeighbourStatusCheckStep runnableStep1 = new NeighbourStatusCheckStep("1");
+            NeighbourStatusCheckStep runnableStep2 = new NeighbourStatusCheckStep("2");
+            NeighbourStatusCheckStep runnableStep3 = new NeighbourStatusCheckStep("3", new[] { runnableStep1, runnableStep2 }); // Multiple dependencies and dependents
+            NeighbourStatusCheckStep runnableStep4 = new NeighbourStatusCheckStep("4", new[] { runnableStep3 });
+            NeighbourStatusCheckStep runnableStep5 = new NeighbourStatusCheckStep("5", new[] { runnableStep3 });
+
+            Pipeline pipeline = new Pipeline(null, new Step[] { runnableStep1, runnableStep2, runnableStep3, runnableStep4, runnableStep5 });
+
+            PipelineRunner pipelineRunner = new PipelineRunner(new StepGraphFactory(), _mockLoggerFactory.Object);
+
+            // Act 
+            pipelineRunner.Run(pipeline, null);
+
+            // Assert
+            Assert.Equal(TaskStatus.RanToCompletion, runnableStep1.Task.Status);
+            Assert.Equal(TaskStatus.RanToCompletion, runnableStep2.Task.Status);
+            Assert.Equal(TaskStatus.RanToCompletion, runnableStep3.Task.Status);
+            Assert.Equal(TaskStatus.RanToCompletion, runnableStep4.Task.Status);
+            Assert.Equal(TaskStatus.RanToCompletion, runnableStep5.Task.Status);
+        }
+
+        private class NeighbourStatusCheckStep : Step
+        {
+            public NeighbourStatusCheckStep(string name, IEnumerable<Step> dependencies = null, bool runEvenIfFaultOccurs = false) :
+                base(name, dependencies, runEvenIfFaultOccurs)
+            { }
+
+            public override void Run(IPipelineContext pipelineContext, CancellationToken cancellationToken)
+            {
+                foreach (Step step in Dependencies)
+                {
+                    Assert.Equal(TaskStatus.RanToCompletion, step.Task.Status);
+                }
+
+                foreach (Step step in Dependents)
+                {
+                    Assert.True(step.Task.Status == TaskStatus.WaitingToRun || step.Task.Status == TaskStatus.WaitingForActivation);
+                }
+            }
+        }
+
+        [Fact]
+        public void Run_TaskStatusAreSetCorrectlyIfATaskThrowsAnException()
         {
             // Arrange
             RunnableStep runnableStep1 = new RunnableStep("r1");
@@ -32,18 +77,36 @@ namespace JeremyTCD.PipelinesCE.Core.Tests.IntegrationTests
 
             Pipeline pipeline = new Pipeline(null, new Step[] { runnableStep1, exceptionThrowingStep, runnableStep2, runnableStep3, runnableStep4 });
 
-            // Act and Assert
-            AggregateException exception = Assert.Throws<AggregateException>(() => _pipelineRunner.Run(pipeline, null));
+            PipelineRunner pipelineRunner = new PipelineRunner(new StepGraphFactory(), _mockLoggerFactory.Object);
 
-            // Assert
-            // TODO create StepStatus enum, cast task status to step status in step status property
+            // Act and Assert
+            AggregateException exception = Assert.Throws<AggregateException>(() => pipelineRunner.Run(pipeline, null));
             Assert.Equal(TaskStatus.RanToCompletion, runnableStep1.Task.Status);
             Assert.Equal(TaskStatus.Faulted, exceptionThrowingStep.Task.Status);
             Assert.Equal(TaskStatus.Canceled, runnableStep2.Task.Status);
             Assert.Equal(TaskStatus.Canceled, runnableStep3.Task.Status);
             Assert.Equal(TaskStatus.RanToCompletion, runnableStep4.Task.Status);
-            // TODO assert that aggregateexception is as expected (create a private dummy exception type)
-            // TODO expost exception through step?  
+            Assert.Equal(3, exception.InnerExceptions.Count);
+            Assert.Equal(1, exception.InnerExceptions.Where(e => e.GetType() == typeof(DummyException)).Count());
+            Assert.Equal(2, exception.InnerExceptions.Where(e => e.GetType() == typeof(TaskCanceledException)).Count());
+        }
+
+        [Fact]
+        public void Cancel_CancelsRun()
+        {
+            // Arrange
+            PipelineRunner pipelineRunner = new PipelineRunner(new StepGraphFactory(), _mockLoggerFactory.Object);
+            RunnableStep runnableStep1 = new RunnableStep("1");
+            ActionStep runnableStep2 = new ActionStep("2", new[] { runnableStep1 }, (pipelineContext, CancellationToken) => pipelineRunner.Cancel());
+            RunnableStep runnableStep3 = new RunnableStep("3", new[] { runnableStep2 });
+
+            Pipeline pipeline = new Pipeline(null, new Step[] { runnableStep1, runnableStep2, runnableStep3 });
+
+            // Act and Assert
+            Assert.Throws<AggregateException>(() => pipelineRunner.Run(pipeline, null));
+            Assert.Equal(TaskStatus.RanToCompletion, runnableStep1.Task.Status);
+            Assert.Equal(TaskStatus.RanToCompletion, runnableStep2.Task.Status);
+            Assert.Equal(TaskStatus.Canceled, runnableStep3.Task.Status);
         }
 
         private class RunnableStep : Step
@@ -65,9 +128,12 @@ namespace JeremyTCD.PipelinesCE.Core.Tests.IntegrationTests
 
             public override void Run(IPipelineContext pipelineContext, CancellationToken cancellationToken)
             {
-                throw new Exception();
+                throw new DummyException();
             }
         }
 
+        private class DummyException : Exception
+        {
+        }
     }
 }
